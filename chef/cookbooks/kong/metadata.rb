b03de029bc66365f9e2bd7e62af0998e334d0525
Alexander Gordeev <agordeev@mirantis.com>
2012-05-25 13:11:30 +0400

    Fix kong cookbook dependencies

--- chef/cookbooks/kong/metadata.rb
+++ chef/cookbooks/kong/metadata.rb
@@ -4,3 +4,4 @@ license          "All rights reserved"
 description      "Installs/Configures kong"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
 version          "0.0.1"
+depends          "keystone"
