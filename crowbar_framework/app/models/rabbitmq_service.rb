3412589edaf33c5a1556acdf269d7ad6b0c8f548
Alexander Gordeev <agordeev@mirantis.com>
2013-03-13 19:10:34 +0400

    Initial commit

---
+++ crowbar_framework/app/models/rabbitmq_service.rb
@@ -0,0 +1,57 @@
+# Copyright 2011, Dell 
+# 
+# Licensed under the Apache License, Version 2.0 (the "License"); 
+# you may not use this file except in compliance with the License. 
+# You may obtain a copy of the License at 
+# 
+#  http://www.apache.org/licenses/LICENSE-2.0 
+# 
+# Unless required by applicable law or agreed to in writing, software 
+# distributed under the License is distributed on an "AS IS" BASIS, 
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
+# See the License for the specific language governing permissions and 
+# limitations under the License. 
+# 
+
+class RabbitmqService < ServiceObject
+
+  def initialize(thelogger)
+    @bc_name = "rabbitmq"
+    @logger = thelogger
+  end
+
+  def self.allow_multiple_proposals?
+    true
+  end
+
+  def proposal_dependencies(role)
+    answer = []
+    answer
+  end
+
+  def create_proposal
+    @logger.debug("Rabbitmq create_proposal: entering")
+    base = super
+    @logger.debug("Rabbitmq create_proposal: done with base")
+
+    nodes = NodeObject.all
+    nodes.delete_if { |n| n.nil? }
+    nodes.delete_if { |n| n.admin? } if nodes.size > 1
+    head = nodes.shift
+    base["deployment"]["rabbitmq"]["elements"] = {
+      "rabbitmq-server" => [ head.name ]
+    }
+
+    @logger.debug("Rabbitmq create_proposal: exiting")
+    base
+  end
+
+  def apply_role_pre_chef_call(old_role, role, all_nodes)
+    @logger.debug("Rabbitmq apply_role_pre_chef_call: entering #{all_nodes.inspect}")
+    return if all_nodes.empty?
+
+    @logger.debug("Rabbitmq apply_role_pre_chef_call: leaving")
+  end
+
+end
+
