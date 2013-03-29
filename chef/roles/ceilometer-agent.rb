70173ca46c1d284c40c66794c9b36da6f64d22ab
Maxim Datskovskiy <axamus@gmail.com>
2013-03-29 18:08:13 +0200

    update roles

--- chef/roles/ceilometer-agent.rb
+++ chef/roles/ceilometer-agent.rb
@@ -1,7 +1,8 @@
 name "ceilometer-agent"
 description "Ceilometer Agent Role"
 run_list(
-         "recipe[ceilometer::agent]"
+         "recipe[ceilometer::agent]",
+         "recipe[ceilometer::common]"
 )
 default_attributes()
 override_attributes()
--- chef/roles/ceilometer-server.rb
+++ chef/roles/ceilometer-server.rb
@@ -1,7 +1,8 @@
 name "ceilometer-server"
 description "Ceilometer Server Role"
 run_list(
-         "recipe[ceilometer::server]"
+         "recipe[ceilometer::server]",
+         "recipe[ceilometer::common]"         
 )
 default_attributes()
 override_attributes()
