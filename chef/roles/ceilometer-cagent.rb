5650048c40207e580ed344dd664ee3685563d40d
Sergey V. Yudin <syudin@mirantis.com>
2013-04-01 15:10:17 +0000

    typo

--- chef/roles/ceilometer-cagent.rb
+++ chef/roles/ceilometer-cagent.rb
@@ -1,7 +1,7 @@
 name "ceilometer-cagent"
 description "Ceilometer Central Agent Role"
 run_list(
-         "recipe[ceilometer::central]"
+         "recipe[ceilometer::central]",
          "recipe[ceilometer::common]"
 )
 default_attributes()
