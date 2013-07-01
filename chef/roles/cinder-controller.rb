77500da0221da6208422432f06d5ae126f7b60a5
Vincent Untz <vuntz@suse.com>
2013-07-01 15:21:52 +0200

    Fix git after splitting roles
    Fixes after c7fc2a6 and 3a26baf got merged:
     - correctly get/set db password
     - merge back cinder-api and cinder-scheduler roles in a
       cinder-controller role, as I suggested.
    
    Current code is broken with the following:
    
    ================================================================================
    Error executing action `start` on resource 'service[openstack-cinder-scheduler]'
    ================================================================================
    
    Mixlib::ShellOut::ShellCommandFailed
    ------------------------------------
    Expected process to exit with [0], but received '1'
    ---- Begin output of /sbin/service openstack-cinder-scheduler start ----
    STDOUT: Checking for DB MigrationsStarting cinder-scheduler..done
    STDERR: 2013-07-01 13:06:40 CRITICAL [cinder] (OperationalError) FATAL:  password authentication failed for user "cinder"
     None None
    ..failed
    ---- End output of /sbin/service openstack-cinder-scheduler start ----
    Ran /sbin/service openstack-cinder-scheduler start returned 1
    
    Resource Declaration:
    ---------------------
    
     21:   service cinder_name do
     22:     if (platform?("ubuntu") && node.platform_version.to_f >= 10.04)
     23:       restart_command "stop #{cinder_name} ; start #{cinder_name}"
     24:       stop_command "stop #{cinder_name}"
     25:       start_command "start #{cinder_name}"
     26:       status_command "status #{cinder_name} | cut -d' ' -f2 | cut -d'/' -f1 | grep start"
     27:     end
     28:     supports :status => true, :restart => true

--- chef/roles/cinder-controller.rb
+++ chef/roles/cinder-controller.rb
@@ -1,7 +1,8 @@
-name "cinder-api"
-description "Cinder API Role"
+name "cinder-controller"
+description "Cinder API and Scheduler Role"
 run_list(
   "recipe[cinder::api]",
+  "recipe[cinder::scheduler]",
   "recipe[cinder::monitor]"
 )
 default_attributes()
