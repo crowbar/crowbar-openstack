48be91fe37e2b99ca1805eaf8c6291952eb99026
Vincent Untz <vuntz@suse.com>
2013-08-08 08:55:07 +0200

    Retry after failure of quantum commands in post_install recipe
    It seems they sometimes fail, but probably because of a race. We retry
    five times, waiting 10 seconds between each retry, in case of failure.

--- chef/cookbooks/quantum/recipes/post_install_conf.rb
+++ chef/cookbooks/quantum/recipes/post_install_conf.rb
@@ -99,25 +99,36 @@ end
 execute "create_fixed_network" do
   command "#{quantum_cmd} net-create fixed --shared #{fixed_network_type}"
   not_if "out=$(#{quantum_cmd} net-list); [ $? != 0 ] || echo ${out} | grep -q ' fixed '"
+  retries 5
+  retry_delay 10
 end
 
 execute "create_floating_network" do
   command "#{quantum_cmd} net-create floating --router:external=True #{floating_network_type}"
   not_if "out=$(#{quantum_cmd} net-list); [ $? != 0 ] || echo ${out} | grep -q ' floating '"
+  retries 5
+  retry_delay 10
 end
 
 execute "create_fixed_subnet" do
   command "#{quantum_cmd} subnet-create --name fixed --allocation-pool start=#{fixed_pool_start},end=#{fixed_pool_end} --gateway #{fixed_router_pool_end} fixed #{fixed_range}"
   not_if "out=$(#{quantum_cmd} subnet-list); [ $? != 0 ] || echo ${out} | grep -q ' fixed '"
+  retries 5
+  retry_delay 10
 end
+
 execute "create_floating_subnet" do
   command "#{quantum_cmd} subnet-create --name floating --allocation-pool start=#{floating_pool_start},end=#{floating_pool_end} --gateway #{public_router} floating #{public_range} --enable_dhcp False"
   not_if "out=$(#{quantum_cmd} subnet-list); [ $? != 0 ] || echo ${out} | grep -q ' floating '"
+  retries 5
+  retry_delay 10
 end
 
 execute "create_router" do
   command "#{quantum_cmd} router-create router-floating ; #{quantum_cmd} router-gateway-set router-floating floating ; #{quantum_cmd} router-interface-add router-floating fixed"
   not_if "out=$(#{quantum_cmd} router-list); [ $? != 0 ] || echo ${out} | grep -q router-floating"
+  retries 5
+  retry_delay 10
 end
 
 def networks_params_equal?(netw1, netw2, keys_list)
