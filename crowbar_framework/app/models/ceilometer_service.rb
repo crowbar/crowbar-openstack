78e091f9d2c5ab721a6c73a2edd654d99f99cfbd
Dirk Mueller <dmueller@suse.com>
2013-08-29 14:24:55 +0200

    Adjust Proposal defaults to changed Nova roles
    Nova roles were renamed to have a hypervisor suffix.

--- crowbar_framework/app/models/ceilometer_service.rb
+++ crowbar_framework/app/models/ceilometer_service.rb
@@ -1,17 +1,17 @@
-# Copyright 2011, Dell 
-# 
-# Licensed under the Apache License, Version 2.0 (the "License"); 
-# you may not use this file except in compliance with the License. 
-# You may obtain a copy of the License at 
-# 
-#  http://www.apache.org/licenses/LICENSE-2.0 
-# 
-# Unless required by applicable law or agreed to in writing, software 
-# distributed under the License is distributed on an "AS IS" BASIS, 
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
-# See the License for the specific language governing permissions and 
-# limitations under the License. 
-# 
+# Copyright 2011, Dell
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#  http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
 
 class CeilometerService < ServiceObject
 
@@ -110,7 +110,11 @@ class CeilometerService < ServiceObject
       raise(I18n.t('model.service.dependency_missing', :name => @bc_name, :dependson => "rabbitmq"))
     end
 
-    agent_nodes = NodeObject.find("roles:nova-multi-compute")
+    agent_nodes = NodeObject.find("roles:nova-multi-compute-kvm") +
+      NodeObject.find("roles:nova-multi-compute-qemu") +
+      NodeObject.find("roles:nova-multi-compute-xen") +
+      NodeObject.find("roles:nova-multi-compute-esxi")
+
     server_nodes = NodeObject.find("roles:nova-multi-controller")
 
     base["deployment"]["ceilometer"]["elements"] = {
