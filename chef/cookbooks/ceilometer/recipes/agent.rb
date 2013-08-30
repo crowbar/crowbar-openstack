fb2e12da48be8dc23a4bd88349476c1de423d173
Dirk Mueller <dmueller@suse.com>
2013-08-30 11:12:14 +0200

    Remove patching of Ceilometer Agent
    This is not needed anymore, current stable/grizzly branch has the
    fix:
    
    https://review.openstack.org/#/c/39483/

--- chef/cookbooks/ceilometer/recipes/agent.rb
+++ chef/cookbooks/ceilometer/recipes/agent.rb
@@ -22,7 +22,7 @@ else
   ceilometer_path = "/opt/ceilometer"
   pfs_and_install_deps(@cookbook_name)
   link_service "ceilometer-agent-compute"
-  create_user_and_dirs(@cookbook_name) 
+  create_user_and_dirs(@cookbook_name)
   execute "cp_policy.json" do
     command "cp #{ceilometer_path}/etc/ceilometer/policy.json /etc/ceilometer"
     creates "/etc/ceilometer/policy.json"
@@ -31,19 +31,6 @@ else
     command "cp #{ceilometer_path}/etc/ceilometer/pipeline.yaml /etc/ceilometer"
     creates "/etc/ceilometer/pipeline.yaml"
   end
-
-  # Remove after https://bugs.launchpad.net/ceilometer/+bug/1179560 is fixed
-  cookbook_file "/usr/lib/python2.7/dist-packages/patch1179560.patch" do
-    path "/usr/lib/python2.7/dist-packages/patch1179560.patch"
-    source "patch1179560.patch"
-    mode 00644
-  end
-  execute "patch bug 1179560" do
-    command "cd /usr/lib/python2.7/dist-packages && patch -p1 < patch1179560.patch && rm patch1179560.patch"
-    action :nothing
-  end
-  # Remove after https://bugs.launchpad.net/ceilometer/+bug/1179560 is fixed
-
 end
 
 include_recipe "#{@cookbook_name}::common"
