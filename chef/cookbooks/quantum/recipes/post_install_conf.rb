f15203a63bc948dbf6802fc2f20de02bbb96287d
Evgeniy Shurmin <eshurmin@mirantis.com>
2013-09-02 13:14:12 +0400

    fix configuring quantum networks and router

--- chef/cookbooks/quantum/recipes/post_install_conf.rb
+++ chef/cookbooks/quantum/recipes/post_install_conf.rb
@@ -96,24 +96,36 @@ when "linuxbridge"
     floating_network_type = "--provider:network_type vlan --provider:segmentation_id #{public_net["vlan"]} --provider:physical_network physnet1"
 end
 
-execute "create_fixed_network" do
-  command "#{quantum_cmd} net-create fixed --shared #{fixed_network_type}"
-  not_if "out=$(#{quantum_cmd} net-list); [ $? != 0 ] || echo ${out} | grep -q ' fixed '"
+execute "Quantum network configuration" do
+  command "true"
+  notifies :run, "execute[create_fixed_network]", :immediately
+  notifies :run, "execute[create_floating_network]", :immediately
+  notifies :run, "execute[create_fixed_subnet]", :immediately
   notifies :run, "execute[create_floating_subnet]", :immediately
+  notifies :run, "execute[create_router]", :immediately
+end.run_action(:run)
+
+execute "create_fixed_network" do
+  command ""
+  not_if "out=$(#{quantum_cmd} net-list); [ $? != 0 ] || echo ${out} | grep -q ' fixed '"
   action :nothing
 end
 
-execute "create_floating_subnet" do
-  command "#{quantum_cmd} subnet-create --name floating --allocation-pool start=#{floating_pool_start},end=#{floating_pool_end} --gateway #{public_router} floating #{public_range} --enable_dhcp False"
-  not_if "out=$(#{quantum_cmd} subnet-list); [ $? != 0 ] || echo ${out} | grep -q ' floating '"
-  notifies :run, "execute[create_fixed_subnet]", :immediately
+execute "create_floating_network" do
+  command "#{quantum_cmd} net-create floating --router:external=True #{floating_network_type}"
+  not_if "out=$(#{quantum_cmd} net-list); [ $? != 0 ] || echo ${out} | grep -q ' floating '"
   action :nothing
 end
 
 execute "create_fixed_subnet" do
   command "#{quantum_cmd} subnet-create --name fixed --allocation-pool start=#{fixed_pool_start},end=#{fixed_pool_end} --gateway #{fixed_router_pool_end} fixed #{fixed_range}"
   not_if "out=$(#{quantum_cmd} subnet-list); [ $? != 0 ] || echo ${out} | grep -q ' fixed '"
-  notifies :run, "execute[create_router]", :immediately
+  action :nothing
+end
+
+execute "create_floating_subnet" do
+  command "#{quantum_cmd} subnet-create --name floating --allocation-pool start=#{floating_pool_start},end=#{floating_pool_end} --gateway #{public_router} floating #{public_range} --enable_dhcp False"
+  not_if "out=$(#{quantum_cmd} subnet-list); [ $? != 0 ] || echo ${out} | grep -q ' floating '"
   action :nothing
 end
 
@@ -121,12 +133,6 @@ execute "create_router" do
   command "#{quantum_cmd} router-create router-floating ; #{quantum_cmd} router-gateway-set router-floating floating ; #{quantum_cmd} router-interface-add router-floating fixed"
   not_if "out=$(#{quantum_cmd} router-list); [ $? != 0 ] || echo ${out} | grep -q router-floating"
   action :nothing
-end
-
-execute "create_floating_network" do
-  command "#{quantum_cmd} net-create floating --router:external=True #{floating_network_type}"
-  not_if "out=$(#{quantum_cmd} net-list); [ $? != 0 ] || echo ${out} | grep -q ' floating '"
-  notifies :run, "execute[create_fixed_network]", :immediately
 end
 
 def networks_params_equal?(netw1, netw2, keys_list)
