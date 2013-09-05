25a6b1211a51c758f94fe8af0434cc41d5aec637
Sergey V. Yudin <tsipa740@gmail.com>
2013-09-05 09:04:06 +0000

    we use an IP address here, and not nova[:fqdn] because nova-metadata doesn't use SSL

--- chef/cookbooks/quantum/recipes/server.rb
+++ chef/cookbooks/quantum/recipes/server.rb
@@ -170,7 +170,10 @@ if novas.length > 0
 else
   nova = node
 end
-metadata_host = nova[:fqdn]
+# we use an IP address here, and not nova[:fqdn] because nova-metadata doesn't use SSL
+# and because it listens on this specific IP address only (so we don't want to use a name
+# that could resolve to 127.0.0.1).
+metadata_host = Chef::Recipe::Barclamp::Inventory.get_network_by_type(nova, "admin").address
 metadata_port = "8775"
 metadata_proxy_shared_secret = (nova[:nova][:quantum_metadata_proxy_shared_secret] rescue '')
 
