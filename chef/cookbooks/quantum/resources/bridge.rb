674c69b31894c01d8ef6f033ecf66199ac026968
Sergey V. Yudin <tsipa740@gmail.com>
2013-10-17 16:52:48 +0000

    renaming

--- chef/cookbooks/quantum/resources/bridge.rb
+++
@@ -1,6 +0,0 @@
-actions :create
-
-attribute :network_name, :kind_of => String, :required => true
-attribute :slaves, :kind_of => Array
-attribute :type, :equal_to => ["linuxbridge"], :required => true
-attribute :quantum_cmd, :kind_of => String, :required => true
--- chef/roles/quantum-server.rb
+++
@@ -1,9 +0,0 @@
-name "quantum-server"
-description "Quantum server"
-
-run_list(
-  "recipe[quantum::server]",
-  "recipe[quantum::monitor]"
-)
-
-override_attributes "quantum" => { "quantum_server" => "true" }
