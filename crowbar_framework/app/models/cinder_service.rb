a375640e21200850e18b03903dd9251660a74858
Evgeniy Shurmin <eshurmin@mirantis.com>
2013-10-21 14:58:18 +0400

    fix to use git instance

--- crowbar_framework/app/models/cinder_service.rb
+++ crowbar_framework/app/models/cinder_service.rb
@@ -50,7 +50,21 @@ class CinderService < ServiceObject
     end
 
     insts = ["Database", "Keystone", "Glance", "Rabbitmq"]
-    insts << "Git" if base["attributes"][@bc_name]["use_gitrepo"]
+
+    base["attributes"][@bc_name]["git_instance"] = ""
+    begin
+      gitService = GitService.new(@logger)
+      gits = gitService.list_active[1]
+      if gits.empty?
+        # No actives, look for proposals
+        gits = gitService.proposals[1]
+      end
+      unless gits.empty?
+        base["attributes"][@bc_name]["git_instance"] = gits[0]
+      end
+    rescue
+      @logger.info("#{@bc_name} create_proposal: no git found")
+    end
 
     insts.each do |inst|
       base["attributes"][@bc_name]["#{inst.downcase}_instance"] = ""
