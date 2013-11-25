c00e0f4d081a1a3fefb6c47d7c90667cc1e5e67c
sergeylevchenko <slevchenko@mirantis.com>
2013-11-25 18:53:32 +0200

    Following changes were made:   1. Adding back monitoring feature for barclamp-heat.   2. Moving "heat-engine" package back to node attributes and "crowbar.yml".

--- chef/roles/heat-server.rb
+++ chef/roles/heat-server.rb
@@ -2,8 +2,8 @@ name "heat-server"
 description "Heat Server Role"
 run_list(
          "recipe[heat::server]",
-         "recipe[heat::common]"
-         
+         "recipe[heat::common]",
+         "recipe[heat::monitor]"
 )
 default_attributes()
 override_attributes()
