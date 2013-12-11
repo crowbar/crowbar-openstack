1baef2cb4c2ec1212c19e82ebe52d4dee51422f0
Jiří Suchomel <jsuchome@suse.cz>
2013-12-11 12:44:55 +0100

    propose network/controller node for neutron server

--- crowbar_framework/app/models/neutron_service.rb
+++ crowbar_framework/app/models/neutron_service.rb
@@ -81,8 +81,11 @@ class NeutronService < ServiceObject
       raise(I18n.t('model.service.dependency_missing', :name => @bc_name, :dependson => "database")) 
     end
 
+    network_node = nodes.find { |n| n if n.intended_role == "network" } 
+    network_node = nodes.find { |n| n if n.intended_role == "controller" } unless network_node
+    network_node = nodes.first[:fqdn] unless network_node
     base["deployment"]["neutron"]["elements"] = {
-        "neutron-server" => [ nodes.first[:fqdn] ]
+        "neutron-server" => [ network_node ]
     } unless nodes.nil? or nodes.length ==0
 
     base["attributes"]["neutron"]["service_password"] = '%012d' % rand(1e12)
