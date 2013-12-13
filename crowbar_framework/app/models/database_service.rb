12b8661f69e74a686ffccfd6be3000c3187b7685
Jiří Suchomel <jsuchome@suse.cz>
2013-12-13 16:10:25 +0100

    use fqdn, not the object

--- crowbar_framework/app/models/database_service.rb
+++ crowbar_framework/app/models/database_service.rb
@@ -32,9 +32,9 @@ class DatabaseService < ServiceObject
     nodes = NodeObject.all
     nodes.delete_if { |n| n.nil? or n.admin? }
     if nodes.size >= 1
-      controller = nodes.find { |n| n if n.intended_role == "controller" } || nodes.first[:fqdn]
+      controller = nodes.find { |n| n.intended_role == "controller" } || nodes.first
       base["deployment"]["database"]["elements"] = {
-        "database-server" => [ controller ]
+        "database-server" => [ controller[:fqdn] ]
       }
     end
 
