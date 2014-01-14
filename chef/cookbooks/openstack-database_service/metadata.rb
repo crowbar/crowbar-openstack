45d00e0264de21ebef20ff3ea2f22ca0a91d728f
Ionuț Arțăriși <iartarisi@suse.cz>
2014-01-14 13:31:24 +0100

    bump cookbook version to 9.0.0 to follow stackforge convention

--- chef/cookbooks/openstack-database_service/metadata.rb
+++ chef/cookbooks/openstack-database_service/metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'crowbar@dell.com'
 license          "Apache 2.0"
 description      'Installs/Configures trove'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
+version          '9.0.0'
 recipe           "openstack-database_service::identity_registration", "Registers Trove endpoints and service with Keystone"
 recipe           "openstack-database_service::api", "Installs API service"
 recipe           "openstack-database_service::conductor", "Installs Conductor service"
