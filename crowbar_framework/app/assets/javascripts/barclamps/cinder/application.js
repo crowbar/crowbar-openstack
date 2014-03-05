7aac8a33c09214d2b29b31def636dffc65cab868
Sascha Peilicke <saschpe@gmx.de>
2014-03-05 17:05:45 +0100

    Don't keep raw and local 'volume_name' in sync
    With multi-backend, they have to be different anyway.

--- crowbar_framework/app/assets/javascripts/barclamps/cinder/application.js
+++ crowbar_framework/app/assets/javascripts/barclamps/cinder/application.js
@@ -39,21 +39,6 @@ $(document).ready(function($) {
 
     $(selector).hide(100).attr('disabled', 'disabled');
     $(current).show(100).removeAttr('disabled');
-
-    switch (value) {
-      case 'local':
-        $('#local_container [name="volume_volume_name"]').attr(
-          'value',
-          $('#raw_container [name="volume_volume_name"]').val()
-        );
-        break;
-      case 'raw':
-        $('#raw_container [name="volume_volume_name"]').attr(
-          'value',
-          $('#local_container [name="volume_volume_name"]').val()
-        );
-        break;
-    }
   }).trigger('change');
 
   $('#volume_netapp_storage_protocol').on('change', function() {
