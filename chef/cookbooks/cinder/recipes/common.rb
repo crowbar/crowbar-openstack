e23083b3cb86dfe31ee9830e3eeee74d2089d52b
Vincent Untz <vuntz@suse.com>
2014-03-18 09:12:52 +0100

    Use helper from database cookbook to get database IP address
    This way we're ready for when HA will be used for database.

--- chef/cookbooks/cinder/recipes/common.rb
+++ chef/cookbooks/cinder/recipes/common.rb
@@ -107,16 +107,8 @@ else
   nova_api_insecure = false
 end
 
-sql_env_filter = " AND database_config_environment:database-config-#{node[:cinder][:database_instance]}"
-sqls = search(:node, "roles:database-server#{sql_env_filter}")
-if sqls.length > 0
-  sql = sqls[0]
-  sql = node if sql.name == node.name
-else
-  sql = node
-end
-
-sql_address = Chef::Recipe::Barclamp::Inventory.get_network_by_type(sql, "admin").address if sql_address.nil?
+sql = get_instance('roles:database-server')
+sql_address = CrowbarDatabaseHelper.get_listen_address(sql)
 Chef::Log.info("SQL server found at #{sql_address}")
 
 include_recipe "database::client"
