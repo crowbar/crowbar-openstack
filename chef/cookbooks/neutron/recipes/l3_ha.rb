a1ca3c4244eb6795e402b53ee56d72b415fd40ea
Ralf Haferkamp <rhafer@suse.de>
2014-03-21 14:41:47 +0100

    Include linux-bridge/ovs-agent into the l3 HA group

--- chef/cookbooks/neutron/recipes/l3_ha.rb
+++ chef/cookbooks/neutron/recipes/l3_ha.rb
@@ -49,11 +49,30 @@ pacemaker_primitive node[:neutron][:platform][:metering_agent_name] do
   retry_delay 5
 end
 
+networking_plugin = node[:neutron][:networking_plugin]
+case networking_plugin
+when "openvswitch", "cisco"
+  neutron_agent = node[:neutron][:platform][:ovs_agent_name]
+when "linuxbridge"
+  neutron_agent = node[:neutron][:platform][:lb_agent_name]
+when "vmware"
+  neutron_agent = node[:neutron][:platform][:nvp_agent_name]
+end
+
+pacemaker_primitive neutron_agent do
+  agent node[:neutron][:ha][:l3]["#{networking_plugin}_ra"]
+  op node[:neutron][:ha][:l3][:op]
+  action [ :create ]
+  retries 1
+  retry_delay 5
+end
+
 group_members = []
 group_members << node[:neutron][:platform][:l3_agent_name] if use_l3_agent
 group_members += [ node[:neutron][:platform][:dhcp_agent_name],
                    node[:neutron][:platform][:metadata_agent_name],
-                   node[:neutron][:platform][:metering_agent_name] ]
+                   node[:neutron][:platform][:metering_agent_name],
+                   neutron_agent ]
 
 pacemaker_group "group-neutron-agents" do
   members group_members
