3cf7353cd29f78cef597f1875b41de078af915d6
Dirk Mueller <dmueller@suse.com>
2014-03-27 08:09:59 +0100

    Readd missing end statement
    this was lost in merge 79bc5b0

--- chef/cookbooks/heat/attributes/default.rb
+++ chef/cookbooks/heat/attributes/default.rb
@@ -31,6 +31,7 @@ case node["platform"]
                     "openstack-heat-api-cfn", "openstack-heat-api-cloudwatch"],
       :aux_dirs => ["/var/cache/heat", "/etc/heat/environment.d"]
     }
+end
 
 default[:heat][:debug] = false
 default[:heat][:verbose] = false
