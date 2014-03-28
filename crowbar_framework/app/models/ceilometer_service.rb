vuntz/barclamp-ceilometer

Configuration of active-pasive HA for ceilometer-agent-central:
Creating cluster resource configuration for pacemaker.

use LSB service until OCF agent is able to use LSB services internally

use PacemakerServiceObject's helper

crowbar_framework/app/models/ceilometer_service.rb
@@ -140,6 +140,10 @@ class CeilometerService < PacemakerServiceObject
     # the VIP of the cluster to be setup
     allocate_virtual_ips_for_any_cluster_in_networks(server_elements, vip_networks)
 
+    central_elements, central_nodes, central_ha_enabled = role_expand_elements(role, "ceilometer-central")
+
+    role.save if prepare_role_for_ha(role, ["ceilometer", "ha", "central", "enabled"], central_ha_enabled)
+
     @logger.debug("Ceilometer apply_role_pre_chef_call: leaving")
   end
 

crowbar/crowbar-openstack

crowbar_framework/app/models/ceilometer_service.rb
@@ -34,7 +34,8 @@ class CeilometerService < PacemakerServiceObject
         },
         "ceilometer-cagent" => {
           "unique" => false,
-          "count" => 1
+          "count" => 1,
+          "cluster" => true
         },
         "ceilometer-server" => {
           "unique" => false,
@@ -139,6 +140,10 @@ class CeilometerService < PacemakerServiceObject
     # No specific need to call sync dns here, as the cookbook doesn't require
     # the VIP of the cluster to be setup
     allocate_virtual_ips_for_any_cluster_in_networks(server_elements, vip_networks)
+
+    central_elements, central_nodes, central_ha_enabled = role_expand_elements(role, "ceilometer-cagent")
+
+    role.save if prepare_role_for_ha(role, ["ceilometer", "ha", "central", "enabled"], central_ha_enabled)
 
     @logger.debug("Ceilometer apply_role_pre_chef_call: leaving")
   end