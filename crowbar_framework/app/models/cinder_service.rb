d64575469cc37dfa4d97985f018a899acdde24df
Balazs Kutil <bkutil@suse.com>
2014-03-31 12:27:32 +0200

    Update drive validation message

--- crowbar_framework/app/models/cinder_service.rb
+++ crowbar_framework/app/models/cinder_service.rb
@@ -86,11 +86,13 @@ class CinderService < PacemakerServiceObject
     validate_at_least_n_for_role proposal, "cinder-volume", 1
 
     if proposal["attributes"][@bc_name]["volume"]["volume_type"] == "raw"
-      has_usable_drives = proposal["deployment"][@bc_name]["elements"]["cinder-volume"].all? do |node_name|
+      nodes_without_unclaimed_drives = proposal["deployment"][@bc_name]["elements"]["cinder-volume"].select do |node_name|
         node = NodeObject.find_node_by_name(node_name)
-        node && !node.unclaimed_physical_drives.empty?
+        node && node.unclaimed_physical_drives.empty?
       end
-      validation_error("All nodes require at least one unclaimed drive for cinder volume backed by a raw device.") unless has_usable_drives
+      unless nodes_without_unclaimed_drives.empty?
+        validation_error("Cinder-volume nodes #{nodes_without_unclaimed_drives.to_sentence} are missing at least one unclaimed disk, required when using raw devices.")
+      end
     end
 
     if proposal["attributes"][@bc_name]["use_gitrepo"]
