b622abbf4aadb4f1e2a0faffddd022d1f496c29b
Vincent Untz <vuntz@suse.com>
2014-04-02 19:52:19 +0200

    Add ability to choose size of DRBD device
    Since it's a LVM volume, we can't guess what's right and it's better to
    leave it up to the user.

--- chef/cookbooks/postgresql/recipes/ha_storage.rb
+++ chef/cookbooks/postgresql/recipes/ha_storage.rb
@@ -42,7 +42,7 @@ if node[:database][:ha][:storage][:mode] == "drbd"
   drbd_resource = "postgresql"
 
   crowbar_pacemaker_drbd drbd_resource do
-    size "50G"
+    size "#{node[:database][:ha][:storage][:drbd][:size]}G"
     action :nothing
   end.run_action(:create)
 
--- crowbar_framework/app/models/database_service.rb
+++ crowbar_framework/app/models/database_service.rb
@@ -77,6 +77,7 @@ class DatabaseService < PacemakerServiceObject
         cluster = servers.first
         role = available_clusters[cluster]
         validation_error("DRBD is not enabled for cluster #{cluster_name(cluster)}.") unless role.default_attributes["pacemaker"]["drbd"]["enabled"]
+        validation_error("Invalid size for DRBD device.") if attributes["ha"]["storage"]["drbd"]["size"] <= 0
       end
     end
 
