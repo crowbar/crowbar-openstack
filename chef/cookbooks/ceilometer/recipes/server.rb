aa74efdf7c8e377538ee0f42dd7eea25273dc5b8
Vincent Untz <vuntz@suse.com>
2014-04-15 09:42:38 +0200

    Only run the db_sync the very first time (on SUSE, at least)

--- chef/cookbooks/ceilometer/recipes/server.rb
+++ chef/cookbooks/ceilometer/recipes/server.rb
@@ -181,14 +181,28 @@ my_public_host = CrowbarHelper.get_host_for_public_url(node, node[:ceilometer][:
 
 crowbar_pacemaker_sync_mark "wait-ceilometer_db_sync"
 
-execute "calling ceilometer-dbsync" do
+execute "ceilometer-dbsync" do
   command "#{venv_prefix}ceilometer-dbsync"
   action :run
   user node[:ceilometer][:user]
   group node[:ceilometer][:group]
   # On SUSE, we only need this when HA is enabled as the init script is doing
-  # this (but that creates races with HA)
-  only_if { node.platform != "suse" || ha_enabled }
+  # this (but that creates races with HA); we only care about it for the
+  # initial sync, though, so we'll do that once, on the founder.
+  only_if { node.platform != "suse" || (!node[:ceilometer][:db_synced] && ha_enabled && CrowbarPacemakerHelper.is_cluster_founder?(node)) }
+end
+
+# We want to keep a note that we've done db_sync, so we don't do it again.
+# If we were doing that outside a ruby_block, we would add the note in the
+# compile phase, before the actual db_sync is done (which is wrong, since it
+# could possibly not be reached in case of errors).
+ruby_block "mark node for ceilometer db_sync" do
+  block do
+    node[:ceilometer][:db_synced] = true
+    node.save
+  end
+  action :nothing
+  subscribes :create, "execute[ceilometer-dbsync]", :immediately
 end
 
 crowbar_pacemaker_sync_mark "create-ceilometer_db_sync"
