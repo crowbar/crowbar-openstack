df1b7b6940f4dc70b780c503c3ce4fff710998c7
Vincent Untz <vuntz@suse.com>
2014-04-15 09:41:25 +0200

    Only run the db_sync the very first time (on SUSE, at least)

--- chef/cookbooks/heat/recipes/server.rb
+++ chef/cookbooks/heat/recipes/server.rb
@@ -302,8 +302,22 @@ execute "heat-manage db_sync" do
   group node[:heat][:group]
   command "#{venv_prefix}heat-manage db_sync"
   # On SUSE, we only need this when HA is enabled as the init script is doing
-  # this (but that creates races with HA)
-  only_if { node.platform != "suse" || ha_enabled }
+  # this (but that creates races with HA); we only care about it for the
+  # initial sync, though, so we'll do that once, on the founder.
+  only_if { node.platform != "suse" || (!node[:heat][:db_synced] && ha_enabled && CrowbarPacemakerHelper.is_cluster_founder?(node)) }
+end
+
+# We want to keep a note that we've done db_sync, so we don't do it again.
+# If we were doing that outside a ruby_block, we would add the note in the
+# compile phase, before the actual db_sync is done (which is wrong, since it
+# could possibly not be reached in case of errors).
+ruby_block "mark node for heat db_sync" do
+  block do
+    node[:heat][:db_synced] = true
+    node.save
+  end
+  action :nothing
+  subscribes :create, "execute[heat-manage db_sync]", :immediately
 end
 
 crowbar_pacemaker_sync_mark "create-heat_db_sync"
