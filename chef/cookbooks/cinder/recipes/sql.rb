e280094a562e2c444e94e22c9138daa20964c49d
Vincent Untz <vuntz@suse.com>
2014-04-15 09:39:12 +0200

    Only run the db_sync the very first time (on SUSE, at least)

--- chef/cookbooks/cinder/recipes/sql.rb
+++ chef/cookbooks/cinder/recipes/sql.rb
@@ -78,8 +78,22 @@ execute "cinder-manage db sync" do
   user node[:cinder][:user]
   group node[:cinder][:group]
   # On SUSE, we only need this when HA is enabled as the init script is doing
-  # this (but that creates races with HA)
-  only_if { node.platform != "suse" || node[:cinder][:ha][:enabled] }
+  # this (but that creates races with HA); we only care about it for the
+  # initial sync, though, so we'll do that once, on the founder.
+  only_if { node.platform != "suse" || (!node[:cinder][:db_synced] && node[:cinder][:ha][:enabled] && CrowbarPacemakerHelper.is_cluster_founder?(node)) }
+end
+
+# We want to keep a note that we've done db_sync, so we don't do it again.
+# If we were doing that outside a ruby_block, we would add the note in the
+# compile phase, before the actual db_sync is done (which is wrong, since it
+# could possibly not be reached in case of errors).
+ruby_block "mark node for cinder db_sync" do
+  block do
+    node[:cinder][:db_synced] = true
+    node.save
+  end
+  action :nothing
+  subscribes :create, "execute[cinder-manage db sync]", :immediately
 end
 
 crowbar_pacemaker_sync_mark "create-cinder_database"
