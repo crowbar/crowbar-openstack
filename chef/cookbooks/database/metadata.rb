741f0436790cae76eba3068e7dce1145ed6cf86a
Ionuț Arțăriși <iartarisi@suse.cz>
2014-05-22 15:21:28 +0200

    update database to latest upstream 2.2.1

--- chef/cookbooks/database/metadata.rb
+++ chef/cookbooks/database/metadata.rb
@@ -1,16 +1,22 @@
-maintainer       "Opscode, Inc."
-maintainer_email "cookbooks@opscode.com"
-license          "Apache 2.0"
-description      "Sets up the database master or slave"
+name             'database'
+maintainer       'Opscode, Inc.'
+maintainer_email 'cookbooks@opscode.com'
+license          'Apache 2.0'
+description      'Sets up the database master or slave'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "1.1.4"
+version          '2.2.1'
 
-recipe "database", "Empty placeholder"
+recipe 'database', 'Empty placeholder'
+recipe 'database::ebs_backup', 'Considered deprecated, older way of backing up EBS volumes'
+recipe 'database::ebs_volume', 'Sets up an EBS volume in EC2 for the database'
+recipe 'database::master', 'Creates application specific user and database'
+recipe 'database::snapshot', 'Locks tables and freezes XFS filesystem for replication, assumes EC2 + EBS'
 
-depends "mysql"
-depends "postgresql"
-depends "crowbar-pacemaker"
+depends 'mysql'
+depends 'postgresql', '>= 1.0.0'
+depends 'xfs'
+depends 'crowbar-pacemaker'
 
-%w{ debian ubuntu centos suse fedora redhat scientific }.each do |os|
+%w(debian ubuntu centos suse fedora redhat scientific amazon).each do |os|
   supports os
 end
