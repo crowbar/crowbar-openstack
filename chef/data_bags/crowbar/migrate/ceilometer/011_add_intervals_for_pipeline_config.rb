2ea84b31b92385738aa6d34123d1d27f09a469f9
Vincent Untz <vuntz@suse.com>
2014-07-09 11:54:39 +0200

    Make interval for disk & network configurable
    The general interval for meters might not be what is wanted for these
    sources.

---
+++ chef/data_bags/crowbar/migrate/ceilometer/011_add_intervals_for_pipeline_config.rb
@@ -0,0 +1,11 @@
+def upgrade ta, td, a, d
+  a['disk_interval'] = a['meters_interval']
+  a['network_interval'] = a['meters_interval']
+  return a, d
+end
+
+def downgrade ta, td, a, d
+  a.delete('disk_interval')
+  a.delete('network_interval')
+  return a, d
+end
