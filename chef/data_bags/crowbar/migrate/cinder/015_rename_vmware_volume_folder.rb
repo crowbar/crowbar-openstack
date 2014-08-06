e5c767c976517dd2966d235c7c5be3d36840ae7b
Vincent Untz <vuntz@suse.com>
2014-08-06 17:47:19 +0200

    Fix upgrade from roxy
    Copying straight from the template is nice, but can break when migrating
    from a long time ago and then renaming a key.

--- chef/data_bags/crowbar/migrate/cinder/015_rename_vmware_volume_folder.rb
+++ chef/data_bags/crowbar/migrate/cinder/015_rename_vmware_volume_folder.rb
@@ -1,5 +1,11 @@
 def upgrade ta, td, a, d
-  a['volume_defaults']['vmware']['volume_folder'] = a['volume_defaults']['vmware']['volume']
+  # Careful: upgrading from roxy: the 'volume' attribute will not even exist,
+  # since migration 014 will have created 'volume_folder' directly. This only
+  # applies to volume_defaults (as roxy didn't have any support for VMWare, so
+  # can't have any such backend.
+  if a['volume_defaults']['vmware']['volume_folder'].nil?
+    a['volume_defaults']['vmware']['volume_folder'] = a['volume_defaults']['vmware']['volume']
+  end
   a['volume_defaults']['vmware'].delete('volume')
 
   a['volumes'].each do |volume|
