5b1ae3eb8879f3a42989c5ee9733298870913a0e
Vincent Untz <vuntz@suse.com>
2014-08-19 14:52:26 +0200

    Fix creation of auth_encryption_key attribute on migration (bnc#892512)
    The template has an empty value which cannot work; we need to generate a
    new value.  Since our previous migration was broken, we add a new one to
    fix the setups that are already broken.

--- chef/data_bags/crowbar/migrate/heat/011_add_auth_encryption_key.rb
+++ chef/data_bags/crowbar/migrate/heat/011_add_auth_encryption_key.rb
@@ -1,5 +1,12 @@
 def upgrade ta, td, a, d
-  a['auth_encryption_key'] = ta['auth_encryption_key']
+  # we use a class variable to set the same key in the proposal and in the
+  # role
+  unless defined?(@@heat_auth_encryption_key)
+    service = ServiceObject.new "fake-logger"
+    @@heat_auth_encryption_key = service.random_password
+  end
+
+  a['auth_encryption_key'] = @@heat_auth_encryption_key
   return a, d
 end
 
