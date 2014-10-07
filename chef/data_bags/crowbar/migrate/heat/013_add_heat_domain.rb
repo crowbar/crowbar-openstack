888948346a08f3081940d3fff13a65abc5039e45
Dirk Mueller <dmueller@suse.com>
2014-10-07 13:33:22 +0200

    Fix stack_domain_admin_password migration
    We forgot to create a random password on migration. Add
    a fixup migration to resolve issues with existing deployments.

--- chef/data_bags/crowbar/migrate/heat/013_add_heat_domain.rb
+++ chef/data_bags/crowbar/migrate/heat/013_add_heat_domain.rb
@@ -1,6 +1,12 @@
 def upgrade ta, td, a, d
+  # we use a class variable to set the same key in the proposal and in the
+  # role
   a['stack_domain_admin'] = ta['stack_domain_admin']
-  a['stack_domain_admin_password'] = ta['stack_domain_admin_password']
+  unless defined?(@@heat_stack_domain_admin_password)
+    service = ServiceObject.new "fake-logger"
+    @@heat_stack_domain_admin_password = service.random_password
+  end
+  a['stack_domain_admin_password'] = @@heat_stack_domain_admin_password
   return a, d
 end
 
