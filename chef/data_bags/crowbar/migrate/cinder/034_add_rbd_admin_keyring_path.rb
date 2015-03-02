fbffc80a9ffad5e0acfa2ecc1a2b80cf002a6b80
Michal Jura <mjura@suse.com>
2015-03-02 13:36:37 +0100

    For external ceph cluster also admin keyring file path should be provided

---
+++ chef/data_bags/crowbar/migrate/cinder/034_add_rbd_admin_keyring_path.rb
@@ -0,0 +1,17 @@
+def upgrade ta, td, a, d
+  a['volume_defaults']['rbd']['admin_keyring'] = ta['volume_defaults']['rbd']['admin_keyring']
+  a['volumes'].each do |volume|
+    next if volume['backend_driver'] != 'rbd'
+    volume['rbd']['admin_keyring'] = ta['volume_defaults']['rbd']['admin_keyring']
+  end
+  return a, d
+end
+
+def downgrade ta, td, a, d
+  a['volume_defaults']['rbd'].delete 'admin_keyring'
+  a['volumes'].each do |volume|
+    next if volume['backend_driver'] != 'rbd'
+    volume['rbd'].delete 'admin_keyring'
+  end
+  return a, d
+end
