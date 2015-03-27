crowbar/crowbar-openstack

commit 970d0ff2026e19aaec3b254a449183a0afeaf362

Make awk regexp more specific when fetching id of tenant (bsc#922695)
https://bugzilla.suse.com/show_bug.cgi?id=922695

chef/cookbooks/neutron/recipes/common_config.rb
@@ -126,7 +126,7 @@ unless nova[:nova].nil? or nova[:nova][:ssl].nil?
     action :nothing
   end.run_action(:wakeup)
 
-  nova_admin_tenant_id = %x[keystone --os_username '#{keystone_settings['admin_user']}' --os_password '#{keystone_settings['admin_password']}' --os_tenant_name '#{keystone_settings['admin_tenant']}' --os_auth_url '#{keystone_settings['internal_auth_url']}' --os_region_name '#{keystone_settings['endpoint_region']}' #{keystone_insecure} tenant-get '#{keystone_settings['service_tenant']}' | awk '/id/  { print $4 }'].chomp
+  nova_admin_tenant_id = %x[keystone --os_username '#{keystone_settings['admin_user']}' --os_password '#{keystone_settings['admin_password']}' --os_tenant_name '#{keystone_settings['admin_tenant']}' --os_auth_url '#{keystone_settings['internal_auth_url']}' --os_region_name '#{keystone_settings['endpoint_region']}' #{keystone_insecure} tenant-get '#{keystone_settings['service_tenant']}' | awk '/^\| *id /  { print $4 }'].chomp
 
   raise("Cannot fetch ID of admin tenant") if nova_admin_tenant_id.nil? || nova_admin_tenant_id.empty?
 

commit c2d0011295b466d495c5a04373bfd470b02edfb5

Fix awk expression that broke in 970d0ff2
Escaping was wrong.

chef/cookbooks/neutron/recipes/common_config.rb
@@ -126,7 +126,7 @@ unless nova[:nova].nil? or nova[:nova][:ssl].nil?
     action :nothing
   end.run_action(:wakeup)
 
-  nova_admin_tenant_id = %x[keystone --os_username '#{keystone_settings['admin_user']}' --os_password '#{keystone_settings['admin_password']}' --os_tenant_name '#{keystone_settings['admin_tenant']}' --os_auth_url '#{keystone_settings['internal_auth_url']}' --os_region_name '#{keystone_settings['endpoint_region']}' #{keystone_insecure} tenant-get '#{keystone_settings['service_tenant']}' | awk '/^\| *id /  { print $4 }'].chomp
+  nova_admin_tenant_id = %x[keystone --os_username '#{keystone_settings['admin_user']}' --os_password '#{keystone_settings['admin_password']}' --os_tenant_name '#{keystone_settings['admin_tenant']}' --os_auth_url '#{keystone_settings['internal_auth_url']}' --os_region_name '#{keystone_settings['endpoint_region']}' #{keystone_insecure} tenant-get '#{keystone_settings['service_tenant']}' | awk '/^\\| *id /  { print $4 }'].chomp
 
   raise("Cannot fetch ID of admin tenant") if nova_admin_tenant_id.nil? || nova_admin_tenant_id.empty?
 