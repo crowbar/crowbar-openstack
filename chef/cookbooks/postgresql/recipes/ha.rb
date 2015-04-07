crowbar/barclamp-database

allow VIP and filesystem to start in parallel
Any unnecessary ordering between the VIP and filesystem resources
can cause problems during the chef-client run.

chef/cookbooks/postgresql/recipes/ha.rb
@@ -83,13 +83,13 @@ if node[:database][:ha][:storage][:mode] == "drbd"
 
   pacemaker_colocation "col-#{service_name}" do
     score "inf"
-    resources [vip_primitive, fs_primitive, service_name]
+    resources "( #{vip_primitive} #{fs_primitive} ) #{service_name}"
     action :create
   end
 
   pacemaker_order "o-#{service_name}" do
     score "Mandatory"
-    ordering "#{vip_primitive} #{fs_primitive} #{service_name}"
+    ordering "( #{vip_primitive} #{fs_primitive} ) #{service_name}"
     action :create
     # This is our last constraint, so we can finally start service_name
     notifies :run, "execute[Cleanup #{service_name} after constraints]", :immediately

crowbar/crowbar-openstack

chef/cookbooks/postgresql/recipes/ha.rb
@@ -83,13 +83,13 @@ if node[:database][:ha][:storage][:mode] == "drbd"
 
   pacemaker_colocation "col-#{service_name}" do
     score "inf"
-    resources [fs_primitive, vip_primitive, service_name]
+    resources "( #{fs_primitive} #{vip_primitive} ) #{service_name}"
     action :create
   end
 
   pacemaker_order "o-#{service_name}" do
     score "Mandatory"
-    ordering "#{fs_primitive} #{vip_primitive} #{service_name}"
+    ordering "( #{fs_primitive} #{vip_primitive} ) #{service_name}"
     action :create
     # This is our last constraint, so we can finally start service_name
     notifies :run, "execute[Cleanup #{service_name} after constraints]", :immediately