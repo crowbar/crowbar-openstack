605633bbec90685154b2681107dc57d96efc4a01
Vincent Untz <vuntz@suse.com>
2015-04-27 14:02:23 +0200

    Disable rescue tests when using docker
    Rescue doesn't make sense for docker and won't be implemented, see
    http://blog-calfonso.rhcloud.com/?p=84

--- chef/cookbooks/tempest/recipes/config.rb
+++ chef/cookbooks/tempest/recipes/config.rb
@@ -342,8 +342,10 @@ EOH
   end
 
   use_docker = true
+  use_rescue = false
 else
   use_docker = false
+  use_rescue = true
 end
 
 # FIXME: should avoid search with no environment in query
@@ -392,6 +394,7 @@ template "#{tempest_conf}" do
     :flavor_ref => flavor_ref,
     :alt_flavor_ref => alt_flavor_ref,
     :nova_api_v3 => nova[:nova][:enable_v3_api],
+    :use_rescue => use_rescue,
     :use_resize => use_resize,
     :use_livemigration => use_livemigration,
     # dashboard settings
