a0f8bd10448193977856537c70c901d527d6118f
Vincent Untz <vuntz@suse.com>
2015-05-11 13:38:59 +0200

    Fix incomplete commit 0dd5d08 for removing rootwrap detection
    Some bits were forgotten.

--- chef/cookbooks/neutron/recipes/common_config.rb
+++ chef/cookbooks/neutron/recipes/common_config.rb
@@ -147,7 +147,6 @@ template "/etc/neutron/neutron.conf" do
       :ssl_ca_file => neutron[:neutron][:ssl][:ca_certs],
       :core_plugin => neutron[:neutron][:networking_plugin],
       :service_plugins => service_plugins,
-      :rootwrap_bin =>  node[:neutron][:rootwrap],
       :use_namespaces => true,
       :allow_overlapping_ips => neutron[:neutron][:allow_overlapping_ips],
       :dvr_enabled => neutron[:neutron][:use_dvr]
