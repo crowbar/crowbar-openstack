a02b8d0959903ac276809478f2a918e9d7276c10
Dirk Mueller <dmueller@suse.com>
2015-06-30 10:39:31 +0200

    Fix admin-ness for domain creation
    admin is only admin on the admin tenant. Also use the proper
    environment variable for authentication.

--- chef/cookbooks/heat/recipes/server.rb
+++ chef/cookbooks/heat/recipes/server.rb
@@ -258,8 +258,8 @@ bash "register heat domain" do
   environment ({
     'OS_USERNAME' => keystone_settings['admin_user'],
     'OS_PASSWORD' => keystone_settings['admin_password'],
-    'OS_TENANT_NAME' => keystone_settings['service_tenant'],
-    'OS_URL' => "#{keystone_settings['protocol']}://#{keystone_settings['internal_url_host']}:#{keystone_settings['service_port']}/v3",
+    'OS_TENANT_NAME' => keystone_settings['admin_tenant'],
+    'OS_AUTH_URL' => "#{keystone_settings['protocol']}://#{keystone_settings['internal_url_host']}:#{keystone_settings['service_port']}/v3",
     'OS_REGION_NAME' => keystone_settings['endpoint_region'],
     'OS_IDENTITY_API_VERSION' => "3"
   })
