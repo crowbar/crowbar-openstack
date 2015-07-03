d773ab42b6da13888fde80fdb51a1122f2c51071
Dirk Mueller <dmueller@suse.com>
2015-07-03 16:55:55 +0200

    Enable the management plugin before starting rabbitmq
    It seems that on SLE12 there is a race between rabbitmq
    starting and the plugins client trying to talk to it.
    Avoid it by enabling the plugin first prior to starting
    rabbitmq.

--- chef/cookbooks/rabbitmq/recipes/default.rb
+++ chef/cookbooks/rabbitmq/recipes/default.rb
@@ -53,16 +53,17 @@ else
   rabbitmq_plugins = "#{RbConfig::CONFIG["libdir"]}/rabbitmq/bin/rabbitmq-plugins"
 end
 
+bash "enabling rabbit management" do
+  environment "HOME" => "/root/"
+  code "#{rabbitmq_plugins} --offline enable rabbitmq_management > /dev/null"
+  not_if "#{rabbitmq_plugins} list -E | grep rabbitmq_management -q", :environment => {"HOME" => "/root/"}
+  notifies :restart, "service[rabbitmq-server]"
+end
+
 service "rabbitmq-server" do
   supports :restart => true, :start => true, :stop => true, :status => true
   action [ :enable, :start ]
   provider Chef::Provider::CrowbarPacemakerService if node[:rabbitmq][:ha][:enabled]
 end
 
-bash "enabling rabbit management" do
-  environment "HOME" => "/root/"
-  code "#{rabbitmq_plugins} enable rabbitmq_management > /dev/null"
-  not_if "#{rabbitmq_plugins} list -E | grep rabbitmq_management -q", :environment => {"HOME" => "/root/"}
-  notifies :restart, "service[rabbitmq-server]"
-end
 
