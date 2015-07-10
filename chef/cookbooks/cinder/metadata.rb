b5765504293cfb4d56264819a38817ec7eaaeed2
Thomas Bechtold <tbechtold@suse.com>
2015-07-10 08:36:15 +0200

    Set cinder cookbook name in metadata
    Needed if the cookbook is used without the crowbar framework. I.e. using
    the cookbook as berkshelf dependency.
    Also update maintainer fields.

--- chef/cookbooks/cinder/metadata.rb
+++ chef/cookbooks/cinder/metadata.rb
@@ -1,5 +1,6 @@
-maintainer       "User Unknown"
-maintainer_email "Unknown@Sample.com"
+name             "cinder"
+maintainer       "Crowbar project"
+maintainer_email "crowbar@googlegroups.com"
 license          "Apache 2.0"
 description      "Installs/Configures Cinder"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.me'))
