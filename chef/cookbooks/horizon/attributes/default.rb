0a951596fbc59ec0fc2f9e21c42a686283badec6
Ralf Haferkamp <rhafer@suse.de>
2015-09-11 16:44:21 +0200

    Revert "Add "use_keystone_v3" attribute for Horizon."
    This basically reverts commit e401626755d549076f4286d5d25e2bb80eebbf39. The
    horizon specific "use_keystone_v3" attribute is no longer needed as we can
    switch keystone protocols for all barclamps in a central place now.

--- chef/cookbooks/horizon/attributes/default.rb
+++ chef/cookbooks/horizon/attributes/default.rb
@@ -50,7 +50,3 @@ node[:horizon][:monitor][:ports] = {}
 default["horizon"]["can_set_mount_point"] = false
 # Display password fields for Nova password injection
 default["horizon"]["can_set_password"] = false
-
-# Transient attribute.  Remove it when all OpenStack is deployed with
-# Keystone v3
-default["horizon"]["use_keystone_v3"] = false
