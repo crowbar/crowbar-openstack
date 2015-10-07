c3f250f83506d1b2d1e265c45aa405ba4e249f89
Ralf Haferkamp <rhafer@suse.de>
2015-10-07 16:25:37 +0200

    Fixup migration for tex backport (again)
    Check for the correct attribute. "multi_domain_support" never was an attribute
    of the keystone proposal.

--- chef/data_bags/crowbar/migrate/keystone/032_add_domain_specific_drivers.rb
+++ chef/data_bags/crowbar/migrate/keystone/032_add_domain_specific_drivers.rb
@@ -1,14 +1,18 @@
 def upgrade ta, td, a, d
-  unless a.has_key? "multi_domain_support"
+  unless a.has_key? "domain_specific_drivers"
     a["domain_specific_drivers"] = ta["domain_specific_drivers"]
+  end
+  unless a.has_key? "domain_config_dir"
     a["domain_config_dir"] = ta["domain_config_dir"]
   end
   return a, d
 end
 
 def downgrade ta, td, a, d
-  unless ta.has_key? "multi_domain_support"
+  unless ta.has_key? "domain_specific_drivers"
     a.delete("domain_specific_drivers")
+  end
+  unless ta.has_key? "domain_config_dir"
     a.delete("domain_config_dir")
   end
   return a, d
