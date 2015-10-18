cb0849c1026d961ccf9167b27d36f9974ee92345
Dirk Mueller <dmueller@suse.com>
2015-10-18 13:12:03 +0200

    Install swift and cinderclient for glance api
    When configuring swift and cinder backends explictely in the
    configuration like we do here, we need to also install all dependencies.

--- chef/cookbooks/glance/recipes/api.rb
+++ chef/cookbooks/glance/recipes/api.rb
@@ -98,6 +98,11 @@ network_settings = GlanceHelper.network_settings(node)
 glance_stores = node.default[:glance][:glance_stores]
 glance_stores += ["glance.store.vmware_datastore.Store"] unless node[:glance][:vsphere][:host].empty?
 
+# glance_stores unconditionally enables cinder and swift stores, so we need
+# to install the necessary clients
+package "python-cinderclient"
+package "python-swiftclient"
+
 directory node[:glance][:filesystem_store_datadir] do
   owner node[:glance][:user]
   group node[:glance][:group]
