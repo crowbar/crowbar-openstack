95b0f46ad115e0b2fec6e85d2b0e4d37bcab0bc2
Vincent Untz <vuntz@suse.com>
2015-12-11 17:25:54 +0100

    postgresql: Install ruby-pg on Debian instead of devpackage to build gem
    The code is assuming the gem is not available on Debian, but there's a
    package, so just install it.

--- chef/cookbooks/postgresql/attributes/default.rb
+++ chef/cookbooks/postgresql/attributes/default.rb
@@ -42,7 +42,7 @@ when "debian"
     default["postgresql"]["server"]["service_name"] = "postgresql"
   end
 
-  default["postgresql"]["client"]["packages"] = ["postgresql-client-#{node['postgresql']['version']}","libpq-dev"]
+  default["postgresql"]["client"]["packages"] = ["postgresql-client-#{node['postgresql']['version']}", "ruby-pg"]
   default["postgresql"]["server"]["packages"] = ["postgresql-#{node['postgresql']['version']}"]
   default["postgresql"]["contrib"]["packages"] = ["postgresql-contrib-#{node['postgresql']['version']}"]
 
