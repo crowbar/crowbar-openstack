crowbar/crowbar-openstack

commit f064ae87757b0d1d5d2f67d76a1f6033009d6dc5

Do not assign Xen-compute role to non-Xen capable hosts
Xen is on SUSE only supported on x86/x86_64, so skip it
on other architectures. Includes a minor refactoring to
cleanup the code.

crowbar_framework/app/models/nova_service.rb
@@ -107,8 +107,10 @@ class NovaService < PacemakerServiceObject
     answer
   end
 
-  def node_platform_supports_xen(node)
-    node[:platform_family] == "suse"
+  def node_supports_xen(node)
+    return false if node[:platform_family] != "suse"
+    return false if node[:block_device].include?("vda")
+    node["kernel"]["machine"] =~ /x86_64/
   end
 
   def node_supports_kvm(node)
@@ -144,7 +146,7 @@ class NovaService < PacemakerServiceObject
     non_hyperv = nodes - hyperv
     kvm = non_hyperv.select { |n| n if node_supports_kvm(n) }
     non_kvm = non_hyperv - kvm
-    xen = non_kvm.select { |n| n unless n[:block_device].include?("vda") or !node_platform_supports_xen(n) }
+    xen = non_kvm.select { |n| n if node_supports_xen(n) }
     qemu = non_kvm - xen
 
     # do not use docker by default

commit 6fc892cf337b2fbedaff6cbac89731a8824017ed

nova: Fix call to non existing method (caused by f064ae87)

crowbar_framework/app/models/nova_service.rb
@@ -267,7 +267,7 @@ class NovaService < PacemakerServiceObject
     elements["nova-compute-xen"].each do |n|
       nodes[n] += 1
       node = NodeObject.find_node_by_name(n)
-      unless node.nil? || node_platform_supports_xen(node)
+      unless node.nil? || node_supports_xen(node)
         validation_error I18n.t(
           "barclamp.#{@bc_name}.validation.xen",
           node_platform: node[:platform],