99c81a87eebf4c60cb65ed22377c5716ebbf90d6
Vincent Untz <vuntz@suse.com>
2016-01-16 14:53:13 +0100

    openstack: Add openstack_pacemaker_controller_only_location_for helper

---
+++ chef/cookbooks/crowbar-openstack/definitions/openstack_pacemaker_controller_only_location_for.rb
@@ -0,0 +1,26 @@
+#
+# Copyright 2016, SUSE
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+define :openstack_pacemaker_controller_only_location_for do
+  resource = params[:name]
+  location_name = "l-#{resource}-controller"
+  pacemaker_location location_name do
+    definition OpenStackHAHelper.controller_only_location(location_name, resource)
+    action :update
+    only_if { CrowbarPacemakerHelper.is_cluster_founder?(node) }
+  end
+  location_name
+end
--- chef/cookbooks/crowbar-openstack/metadata.rb
+++ chef/cookbooks/crowbar-openstack/metadata.rb
@@ -6,4 +6,5 @@ description "Help install/configure OpenStack, deployed by Crowbar"
 long_description IO.read(File.join(File.dirname(__FILE__), "README.md"))
 version "0.1"
 
+depends "crowbar-pacemaker"
 depends "database"
