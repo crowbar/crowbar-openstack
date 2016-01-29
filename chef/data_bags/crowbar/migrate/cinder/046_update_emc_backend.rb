7306f74be64acace9cf9628078b6423d3eb8b110
Maximilian Meister <mmeister@suse.de>
2016-01-29 17:10:56 +0100

    fix typo in cinder migration

--- chef/data_bags/crowbar/migrate/cinder/046_update_emc_backend.rb
+++ chef/data_bags/crowbar/migrate/cinder/046_update_emc_backend.rb
@@ -5,7 +5,7 @@ def upgrade(ta, td, a, d)
   attr_emc["ecom_server_portgroups"] = templ_attr_emc["ecom_server_portgroups"]
   attr_emc["ecom_server_array"] = templ_attr_emc["ecom_server_array"]
   attr_emc["ecom_server_pool"] = templ_attr_emc["ecom_server_pool"]
-  attr_emc["ecom_server_polcy"] = templ_attr_emc["ecom_server_policy"]
+  attr_emc["ecom_server_policy"] = templ_attr_emc["ecom_server_policy"]
   attr_emc.delete("emc_storage_type")
   attr_emc.delete("masking_view")
 
@@ -14,7 +14,7 @@ def upgrade(ta, td, a, d)
     volume["emc"]["ecom_server_portgroups"] = templ_attr_emc["ecom_server_portgroups"]
     volume["emc"]["ecom_server_array"] = templ_attr_emc["ecom_server_array"]
     volume["emc"]["ecom_server_pool"] = templ_attr_emc["ecom_server_pool"]
-    volume["emc"]["ecom_server_polcy"] = templ_attr_emc["ecom_server_policy"]
+    volume["emc"]["ecom_server_policy"] = templ_attr_emc["ecom_server_policy"]
     volume["emc"].delete("emc_storage_type")
     volume["emc"].delete("masking_view")
   end
@@ -28,7 +28,7 @@ def downgrade(ta, td, a, d)
   attr_emc.delete("ecom_server_portgroups")
   attr_emc.delete("ecom_server_array")
   attr_emc.delete("ecom_server_pool")
-  attr_emc.delete("ecom_server_polcy")
+  attr_emc.delete("ecom_server_policy")
   attr_emc["emc_storage_type"] = templ_attr_emc["emc_storage_type"]
   attr_emc["masking_view"] = templ_attr_emc["masking_view"]
 
@@ -37,7 +37,7 @@ def downgrade(ta, td, a, d)
     volume["emc"].delete("ecom_server_portgroups")
     volume["emc"].delete("ecom_server_array")
     volume["emc"].delete("ecom_server_pool")
-    volume["emc"].delete("ecom_server_polcy")
+    volume["emc"].delete("ecom_server_policy")
     volume["emc"]["emc_storage_type"] = templ_attr_emc["emc_storage_type"]
     volume["emc"]["masking_view"] = templ_attr_emc["masking_view"]
   end
