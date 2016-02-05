1744d7944689147f47c22ed79087c6eff937928a
Ralf Haferkamp <rhafer@suse.de>
2016-02-05 16:30:36 +0100

    Add migration to switch to uuid tokens during upgrade
    CVE-2015-7546 (https://wiki.openstack.org/wiki/OSSN/OSSN-0062) strongly
    recommends to switch away from PKI/PKIZ tokens because of a security issue
    with revoked tokes.

---
+++ chef/data_bags/crowbar/migrate/keystone/035_pki_uuid_switch.rb
@@ -0,0 +1,15 @@
+def upgrade(ta, td, a, d)
+  # This migration (if part of the release) will only run when upgrading
+  # from Tex. Switch keystone to use UUID tokens after the upgrade for those
+  # systems
+  if a["signing"]["token_format"] == "PKI"
+    a["signing"]["token_format"] = ta["signing"]["token_format"]
+  end
+  return a, d
+end
+
+def downgrade(ta, td, a, d)
+  # There is no good way to roll back the above change since we
+  # don't know what was set before here.
+  return a, d
+end
