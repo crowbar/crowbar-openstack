d814854bcee3ca58b96b954be8f7894fe6e2b664
Vincent Untz <vuntz@suse.com>
2016-02-08 23:00:11 +0100

    manila: Also make path_to_private_key attribute required

--- crowbar_framework/app/models/manila_service.rb
+++ crowbar_framework/app/models/manila_service.rb
@@ -134,7 +134,7 @@ end
         end
         # there must be a private ssh key path or a password
         unless ["service_instance_password", "path_to_private_key"].any? do |s|
-          share[backend_driver].key?(s) && !share[backend_driver][s].empty?
+          !share[backend_driver][s].empty?
         end
           validation_error I18n.t(
             "barclamp.#{@bc_name}.validation.generic.password_or_private_key")
