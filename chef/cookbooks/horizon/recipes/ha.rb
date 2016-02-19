474e05fe88aa8d529caa05222daf850d682696ea
Alberto Planas <aplanas@gmail.com>
2016-02-19 15:33:59 +0100

    ha-horizon: use different loadbalancer options

--- chef/cookbooks/horizon/recipes/ha.rb
+++ chef/cookbooks/horizon/recipes/ha.rb
@@ -18,6 +18,8 @@ haproxy_loadbalancer "horizon" do
   port 80
   use_ssl false
   servers CrowbarPacemakerHelper.haproxy_servers_for_service(node, "horizon", "horizon-server", "plain")
+  options ["defaults", "httpchk"]
+  check ({ inter: 1000, downinter: 3000, rise: 3, fall: 1 })
   action :nothing
 end.run_action(:create)
 
@@ -27,6 +29,7 @@ if node[:horizon][:apache][:ssl]
     port 443
     use_ssl true
     servers CrowbarPacemakerHelper.haproxy_servers_for_service(node, "horizon", "horizon-server", "ssl")
+    check ({ inter: 1000, downinter: 3000, rise: 3, fall: 1 })
     action :nothing
   end.run_action(:create)
 end
