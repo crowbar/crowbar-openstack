687a3e1b099421140343707877c06e766cab0b66
Vincent Untz <vuntz@suse.com>
2016-06-02 10:06:08 +0200

    nova: Fix searches to be based on roles, not recipes
    This broke with the move to role recipes.
    
    (cherry picked from commit 91cbfe857692b7881da7a06094bb7981046c3fbb)

--- chef/cookbooks/nova/recipes/api.rb
+++ chef/cookbooks/nova/recipes/api.rb
@@ -28,7 +28,7 @@ nova_package "objectstore" do
   use_pacemaker_provider node[:nova][:ha][:enabled]
 end
 
-apis = search_env_filtered(:node, "recipes:nova\\:\\:api")
+apis = search_env_filtered(:node, "roles:nova-controller")
 if apis.length > 0
   api = apis[0]
   api = node if api.name == node.name
