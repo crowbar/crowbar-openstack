6d9454a6c644b2cded708abb0cb2bbb74033ff92
Vincent Untz <vuntz@suse.com>
2016-06-02 11:38:02 +0200

    neutron: Add ability to use LBaaSv2
    LBaaSv2 is not experimental anymore in Liberty.
    
    This is disabled by default to stay compatible with the previous
    default.
    
    (cherry picked from commit 1a970f24128dc481d021f6bf374ea70e7d824c06)

--- chef/cookbooks/neutron/recipes/server.rb
+++ chef/cookbooks/neutron/recipes/server.rb
@@ -214,6 +214,7 @@ if node[:neutron][:use_lbaas]
     mode "0640"
     variables(
       interface_driver: interface_driver,
+      use_lbaasv2: node[:neutron][:use_lbaasv2],
       keystone_settings: keystone_settings
     )
   end
