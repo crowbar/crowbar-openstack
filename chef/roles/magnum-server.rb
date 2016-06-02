7b029766ff6aa3ba1d695ab16ac194b95733d411
Madhu Mohan Nelemane <mmnelemane@suse.com>
2016-06-02 16:07:55 +0200

    Use role recipe for magnum-server & little cleanup

--- chef/roles/magnum-server.rb
+++ chef/roles/magnum-server.rb
@@ -1,11 +1,5 @@
 name "magnum-server"
 description "Magnum Role - Node registered as a Magnum server"
-run_list(
-  "recipe[magnum::setup]",
-  "recipe[magnum::common]",
-  "recipe[magnum::sql]",
-  "recipe[magnum::api]",
-  "recipe[magnum::conductor]"
-)
+run_list("recipe[magnum::role_magnum_server]")
 default_attributes
 override_attributes
