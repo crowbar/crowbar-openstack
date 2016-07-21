38bd3158d091f60b887681c4c1d08b095dbdfada
Thomas Bechtold <tbechtold@suse.com>
2016-07-21 11:45:41 +0200

    trove: Move more function to helper lib
    In preparation for the api move from openstack-database to trove.

--- chef/cookbooks/trove/libraries/helpers.rb
+++ chef/cookbooks/trove/libraries/helpers.rb
@@ -46,5 +46,81 @@ class Chef
       end
       rabbitmq_trove_settings
     end
+
+    def get_nova_details
+      # get nova information
+      nova_controllers = search(:node, "roles:nova-controller") || []
+      unless nova_controllers.empty?
+        nova = nova_controllers[0]
+        keystone_settings = KeystoneHelper.keystone_settings(node, @cookbook_name)
+        nova_api_host = CrowbarHelper.get_host_for_admin_url(
+          nova, (nova[:nova][:ha][:enabled] rescue false))
+        nova_api_protocol = nova[:nova][:ssl][:enabled] ? "https" : "http"
+        nova_url = "#{nova_api_protocol}://#{nova_api_host}:#{nova[:nova][:ports][:api]}/v2/"
+        nova_insecure = keystone_settings["insecure"] || (
+          nova[:nova][:ssl][:enabled] && nova[:nova][:ssl][:insecure]
+        )
+      else
+        nova_url = nil
+        nova_insecure = false
+      end
+      [nova_url, nova_insecure]
+    end
+
+    def get_cinder_details
+      # get cinder information
+      cinder_controllers = search(:node, "roles:cinder-controller") || []
+      unless cinder_controllers.empty?
+        cinder = cinder_controllers[0]
+        cinder_api_host = CrowbarHelper.get_host_for_admin_url(
+          cinder, (cinder[:cinder][:ha][:enabled] rescue false)
+        )
+        cinder_api_protocol = cinder[:cinder][:ssl][:enabled] ? "https" : "http"
+        cinder_port = cinder[:cinder][:api][:bind_port]
+        cinder_url = "#{cinder_api_protocol}://#{cinder_api_host}:#{cinder_port}/v1/"
+        cinder_insecure = cinder[:cinder][:api][:protocol] == "https" &&
+                          cinder[:cinder][:ssl][:insecure]
+      else
+        cinder_url = nil
+        cinder_insecure = false
+      end
+      [cinder_url, cinder_insecure]
+    end
+
+    def get_objectstore_details
+      # get swift information
+      swift_proxies = search(:node, "roles:swift-proxy") || []
+      unless swift_proxies.empty?
+        swift = swift_proxies[0]
+        swift_api_host = CrowbarHelper.get_host_for_admin_url(
+          swift, (swift[:swift][:ha][:enabled] rescue false)
+        )
+        swift_api_protocol = swift[:swift][:ssl][:enabled] ? "https" : "http"
+        swift_api_port = swift[:swift][:ports][:api]
+        object_store_url = "#{swift_api_protocol}://#{swift_api_host}:#{swift_api_port}/v1/"
+        object_store_insecure = swift["swift"]["ssl"]["insecure"]
+      else
+        # maybe radosgw instead of swift?
+        ceph_radosgws = search(:node, "roles:ceph-radosgw") || []
+        unless ceph_radosgws.empty?
+          radosgw = ceph_radosgws[0]
+          radosgw_api_host = CrowbarHelper.get_host_for_admin_url(
+            radosgw, (radosgw[:ceph][:ha][:radosgw][:enabled] rescue false)
+          )
+          radosgw_api_protocol = radosgw[:ceph][:radosgw][:ssl][:enabled] ? "https" : "http"
+          if radosgw[:ceph][:radosgw][:ssl][:enabled]
+            radosgw_api_port =  node[:ceph][:radosgw][:rgw_port_ssl]
+          else
+            radosgw_api_port = node[:ceph][:radosgw][:rgw_port]
+          end
+          object_store_url = "#{radosgw_api_protocol}://#{radosgw_api_host}:#{radosgw_api_port}/swift/v1"
+          object_store_insecure = radosgw[:ceph][:radosgw][:ssl][:insecure]
+        else
+          object_store_url = nil
+          object_store_insecure = false
+        end
+        [object_store_url, object_store_insecure]
+      end
+    end
   end
 end
