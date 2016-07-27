1275c7e434f38ae6abc4c26540b7173c52c5a118
Nicolas Bock <nicolas.bock@suse.de>
2016-07-27 15:36:42 +0200

    Satisfy Hound
    (cherry picked from commit 264cf2bf361224eeded0fa0131d794217fcf0ec7)

--- chef/data_bags/crowbar/migrate/cinder/048_nfs_backend.rb
+++ chef/data_bags/crowbar/migrate/cinder/048_nfs_backend.rb
@@ -1,9 +1,9 @@
-def upgrade ta, td, a, d
+def upgrade(ta, td, a, d)
   a["volume_defaults"]["nfs"] = ta["volume_defaults"]["nfs"]
   return a, d
 end
 
-def downgrade ta, td, a, d
+def downgrade(ta, td, a, d)
   a["volume_defaults"].delete("nfs")
   return a, d
 end
