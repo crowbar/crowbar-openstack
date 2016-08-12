bcfb762b7500584278207c87069dfecb2362a804
Nicolas Bock <nicolas.bock@suse.de>
2016-08-12 14:45:45 +0200

    Add correct nfs backend driver

--- chef/data_bags/crowbar/migrate/cinder/101_nfs_backend.rb
+++ chef/data_bags/crowbar/migrate/cinder/101_nfs_backend.rb
@@ -1,9 +1,10 @@
 def upgrade(ta, td, a, d)
-  a["volume_defaults"]["nfs"] = ta["volume_defaults"]["nfs"]
+  a["volume_defaults"]["nfs"] = ta["volume_defaults"]["nfs"] \
+    unless a["volume_defaults"].key?("nfs")
   return a, d
 end
 
 def downgrade(ta, td, a, d)
-  a["volume_defaults"].delete("nfs")
+  a["volume_defaults"].delete("nfs") if a["volume_defaults"].key?("nfs")
   return a, d
 end
