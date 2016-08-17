19cdbd700fb279e93bb61b6fcb3b304ac8f206ee
Thomas Bechtold <tbechtold@suse.com>
2016-08-17 10:57:30 +0200

    Revert "Cinder nfs backend 3.0"

--- chef/data_bags/crowbar/migrate/cinder/101_nfs_backend.rb
+++
@@ -1,9 +0,0 @@
-def upgrade(ta, td, a, d)
-  a["volume_defaults"]["nfs"] = ta["volume_defaults"]["nfs"]
-  return a, d
-end
-
-def downgrade(ta, td, a, d)
-  a["volume_defaults"].delete("nfs")
-  return a, d
-end
