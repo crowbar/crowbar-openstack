c0b0f184c810050f671353a6a85d340277314c1f
Thomas Bechtold <tbechtold@suse.com>
2016-10-19 08:24:07 +0200

    Use "service" resource agent where "lsb" is used
    The "service" resource agent intelligently figures out which system service
    is used. It tries first LSB and then systemd.
    This is useful while we transition to systemd services in our packages.

--- chef/cookbooks/magnum/attributes/default.rb
+++ chef/cookbooks/magnum/attributes/default.rb
@@ -28,8 +28,8 @@ default[:magnum][:ha][:enabled] = false
 default[:magnum][:ha][:ports][:api] = 5611
 # pacemaker definitions
 default[:magnum][:ha][:api][:op][:monitor][:interval] = "10s"
-default[:magnum][:ha][:api][:agent] = "lsb:openstack-magnum-api"
+default[:magnum][:ha][:api][:agent] = "service:openstack-magnum-api"
 default[:magnum][:ha][:conductor][:op][:monitor][:interval] = "10s"
-default[:magnum][:ha][:conductor][:agent] = "lsb:openstack-magnum-conductor"
+default[:magnum][:ha][:conductor][:agent] = "service:openstack-magnum-conductor"
 
 default[:magnum][:cert][:cert_manager_type] = "local"
