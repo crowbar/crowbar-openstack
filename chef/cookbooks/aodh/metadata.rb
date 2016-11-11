4a1ecef34a7b46139840bd35b32be44cf3e4627e
Nicolas Bock <nicolas.bock@suse.de>
2016-11-11 14:46:25 -0700

    Complete renaming of READMEs
    Commit 4c24cb3d010e8ec392cfba57ac3c2bd7f087462a was missing updates in
    the metadata of the affect cookbooks.

--- chef/cookbooks/aodh/metadata.rb
+++ chef/cookbooks/aodh/metadata.rb
@@ -2,5 +2,5 @@ maintainer "Crowbar project"
 maintainer_email "crowbar@googlegroups.com"
 license "Apache 2.0"
 description "Installs/Configures Aodh"
-long_description IO.read(File.join(File.dirname(__FILE__), "README.me"))
+long_description IO.read(File.join(File.dirname(__FILE__), "README.md"))
 version "0.1"
