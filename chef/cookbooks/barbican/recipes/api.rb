9433bc51cf525398abca8028c83527757069aa26
Dirk Mueller <dmueller@suse.com>
2016-11-17 22:23:39 +0100

    barbican: use VIP for endpoints in OpenStack catalog
    In the HA case, the api should only be accessed via the VIP
    otherwise the servise is not really highly available.

--- chef/cookbooks/barbican/recipes/api.rb
+++ chef/cookbooks/barbican/recipes/api.rb
@@ -38,8 +38,10 @@ network_settings = BarbicanHelper.network_settings(node)
 
 bind_port = network_settings[:api][:bind_port]
 barbican_port = node[:barbican][:api][:bind_port]
-admin_host = CrowbarHelper.get_host_for_admin_url(node, false)
-public_host = CrowbarHelper.get_host_for_public_url(node, false, false)
+admin_host = CrowbarHelper.get_host_for_admin_url(node, node[:barbican][:ha][:enabled])
+public_host = CrowbarHelper.get_host_for_public_url(node,
+                                                    node[:barbican][:api][:ssl],
+                                                    node[:barbican][:ha][:enabled])
 register_auth_hash = { user: keystone_settings["admin_user"],
                        password: keystone_settings["admin_password"],
                        tenant: keystone_settings["admin_tenant"] }
