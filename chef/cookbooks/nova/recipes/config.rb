0e0e2bf42a145bf8064b10f8e7609740b63354d1
Vincent Untz <vuntz@suse.com>
2016-12-17 08:20:14 +0100

    nova: Do not search for a controller node if we're one of them

--- chef/cookbooks/nova/recipes/config.rb
+++ chef/cookbooks/nova/recipes/config.rb
@@ -65,12 +65,10 @@ else
   api_database_connection = nil
 end
 
-apis = search_env_filtered(:node, "roles:nova-controller")
-if apis.length > 0
-  api = apis[0]
-  api = node if api.name == node.name
+api = if node["roles"].include?("nova-controller")
+  node
 else
-  api = node
+  search_env_filtered(:node, "roles:nova-controller").first
 end
 
 # use nova-rootwrap daemon on compute-only nodes
