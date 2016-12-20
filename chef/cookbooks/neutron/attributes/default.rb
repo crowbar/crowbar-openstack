faebad8e080c537103354ed82de8ca2f4870a0a6
Steve Kowalik <steven@wedontsleep.org>
2016-12-20 21:02:23 +1100

    neutron: Shuffle config files under conf.d directory
    Move neutron.conf and associated friends to be under
    /etc/neutron/neutron.conf.d

--- chef/cookbooks/neutron/attributes/default.rb
+++ chef/cookbooks/neutron/attributes/default.rb
@@ -23,6 +23,9 @@ default[:neutron][:max_header_line] = 16384
 default[:neutron][:dhcp_domain] = "openstack.local"
 default[:neutron][:networking_plugin] = "ml2"
 default[:neutron][:additional_external_networks] = []
+default[:neutron][:config_file] = "/etc/neutron/neutron.conf.d/100-neutron.conf"
+default[:neutron][:lbaas_service_file] = "/etc/neutron/neutron.conf.d/110-neutron_service_lbaas.conf"
+default[:neutron][:lbaas_config_file] = "/etc/neutron/neutron.conf.d/111-neutron_lbaas.conf"
 
 default[:neutron][:db][:database] = "neutron"
 default[:neutron][:db][:user] = "neutron"
