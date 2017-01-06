0beb8f8d2b23d15ab239ce8b94ad12338ca726eb
Vincent Untz <vuntz@suse.com>
2017-01-06 16:41:11 +0100

    cookbooks: Add missing dependencies to cookbooks
    This matters if these services are installed on nodes where nothing else
    is installed.

--- chef/cookbooks/ec2-api/metadata.rb
+++ chef/cookbooks/ec2-api/metadata.rb
@@ -5,3 +5,9 @@ license "Apache 2.0"
 description "Installs/Configures EC2 API"
 long_description IO.read(File.join(File.dirname(__FILE__), "README.md"))
 version "0.1"
+
+depends "database"
+depends "crowbar-openstack"
+depends "crowbar-pacemaker"
+depends "keystone"
+depends "utils"
