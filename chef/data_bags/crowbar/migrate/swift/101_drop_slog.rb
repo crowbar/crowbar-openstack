6a42195bacb0d43a6eccabd051fe6b8d1055c775
Jiří Suchomel <jiri.suchomel@suse.com>
2017-01-28 10:43:48 +0100

    swift: Use correct slog_passwd key to drop during migration

--- chef/data_bags/crowbar/migrate/swift/101_drop_slog.rb
+++ chef/data_bags/crowbar/migrate/swift/101_drop_slog.rb
@@ -1,12 +1,12 @@
 def upgrade(ta, td, a, d)
-  %w(use_slog slog_account slog_user slog_password).each do |attr|
+  %w(use_slog slog_account slog_user slog_passwd).each do |attr|
     a.delete(attr)
   end
   return a, d
 end
 
 def downgrade(ta, td, a, d)
-  %w(use_slog slog_account slog_user slog_password).each do |attr|
+  %w(use_slog slog_account slog_user slog_passwd).each do |attr|
     a[attr] = ta[attr]
   end
   return a, d
