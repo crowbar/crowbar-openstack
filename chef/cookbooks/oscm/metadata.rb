33f627dfe6ca7020f726b41471de870b186ef994
stavreva <stavreva@eststavreva.intern.est.fujitsu.com>
2017-03-21 11:28:16 +0100

    Fix oscm json  and schema

--- chef/cookbooks/oscm/metadata.rb
+++ chef/cookbooks/oscm/metadata.rb
@@ -6,8 +6,4 @@ description "Installs/Configures OSCM"
 long_description IO.read(File.join(File.dirname(__FILE__), "README.md"))
 version "0.1"
 
-depends "database"
-depends "keystone"
-depends "crowbar-openstack"
-depends "crowbar-pacemaker"
-depends "utils"
+depends "heat"
