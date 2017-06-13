d6574c0f237bbfd05850ee191fc54381522f4cb4
Jiří Suchomel <jiri.suchomel@suse.com>
2017-06-13 08:18:07 +0200

    cinder: Move conflicting migration forward one number
    We've merged 2 different changes, both with migration number 107.
    So to fix the conflict, I'm moving one of these original 107's to 108
    while also adapting the code a bit with some checks for the case user
    has already ran original migration script before.

--- chef/data_bags/crowbar/migrate/cinder/108_remove_nfs_mount_options.rb
+++ chef/data_bags/crowbar/migrate/cinder/108_remove_nfs_mount_options.rb
@@ -1,8 +1,12 @@
 def upgrade(ta, td, a, d)
-  a["volume_defaults"]["nfs"].delete("nfs_mount_options")
+  if a["volume_defaults"]["nfs"].key? "nfs_mount_options"
+    a["volume_defaults"]["nfs"].delete("nfs_mount_options")
+  end
   a["volumes"].each do |volume|
     next if volume["backend_driver"] != "nfs"
-    volume["nfs"].delete("nfs_mount_options")
+    if volume["nfs"].key? "nfs_mount_options"
+      volume["nfs"].delete("nfs_mount_options")
+    end
   end
   return a, d
 end
