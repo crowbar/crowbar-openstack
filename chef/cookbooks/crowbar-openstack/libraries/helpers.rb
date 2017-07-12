a9d47477d0e604abf3c3e2b575b9c55359f75f7a
Itxaka <igarcia@suse.com>
2017-07-12 10:56:54 +0200

    rabbit: set the proper format for the transport_url

--- chef/cookbooks/crowbar-openstack/libraries/helpers.rb
+++ chef/cookbooks/crowbar-openstack/libraries/helpers.rb
@@ -129,18 +129,22 @@ class CrowbarOpenStackHelper
           one_rabbit[:rabbitmq][:ssl][:client_ca_certs]
         end
 
-        if rabbits.first[:rabbitmq][:cluster]
+        if one_rabbit[:rabbitmq][:cluster]
+          # transport_url format:
+          # https://docs.openstack.org/oslo.messaging/latest/reference/transport.html#oslo_messaging.TransportURL
           rabbit_hosts = rabbits.map do |rabbit|
             port = if rabbit[:rabbitmq][:ssl][:enabled]
               rabbit[:rabbitmq][:ssl][:port]
             else
               rabbit[:rabbitmq][:port]
             end
+            url = "#{rabbit[:rabbitmq][:user]}:"
+            url << "#{rabbit[:rabbitmq][:password]}@"
+            url << "#{rabbit[:rabbitmq][:address]}:#{port}"
+            url << "/#{rabbit[:rabbitmq][:vhost]}" unless rabbit.equal? rabbits.first
+            url.prepend("rabbit://") if rabbit.equal? rabbits.first
 
-            "#{rabbit[:rabbitmq][:user]}:" \
-            "#{rabbit[:rabbitmq][:password]}@" \
-            "#{rabbit[:rabbitmq][:address]}:#{port}/" \
-            "#{rabbit[:rabbitmq][:vhost]}"
+            url
           end
 
           trove_rabbit_hosts = rabbits.map do |rabbit|
@@ -150,10 +154,11 @@ class CrowbarOpenStackHelper
               rabbit[:rabbitmq][:port]
             end
 
-            "#{rabbit[:rabbitmq][:trove][:user]}:" \
-            "#{rabbit[:rabbitmq][:trove][:password]}@" \
-            "#{rabbit[:rabbitmq][:address]}:#{port}/" \
-            "#{rabbit[:rabbitmq][:trove][:vhost]}"
+            url = "#{rabbit[:rabbitmq][:trove][:user]}:"
+            url << "#{rabbit[:rabbitmq][:trove][:password]}@"
+            url << "#{rabbit[:rabbitmq][:address]}:#{port}"
+            url << "/#{rabbit[:rabbitmq][:trove][:vhost]}" unless rabbit.equal? rabbits.first
+            url.prepend("rabbit://") if rabbit.equal? rabbits.first
           end
 
           @rabbitmq_settings[instance] = {
@@ -161,8 +166,8 @@ class CrowbarOpenStackHelper
             durable_queues: true,
             use_ssl: one_rabbit[:rabbitmq][:ssl][:enabled],
             client_ca_certs: client_ca_certs,
-            url: "rabbit://#{rabbit_hosts.sort.join(", ")}",
-            trove_url: "rabbit://#{trove_rabbit_hosts.sort.join(", ")}",
+            url: rabbit_hosts.join(","),
+            trove_url: trove_rabbit_hosts.join(","),
             pacemaker_resource: "ms-rabbitmq"
           }
           Chef::Log.info("RabbitMQ cluster found")
