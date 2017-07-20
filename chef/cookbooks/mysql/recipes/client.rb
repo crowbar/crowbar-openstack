af683f84954c895f7f5fb9151037826b9101d71e
Rick Salevsky <rsalevsky@suse.de>
2017-07-20 16:07:39 +0200

    mysql: Switch to mysql2 to support newer mariadb versions (SCRD-593)
    The rubygem-mysql got deprecated by rubygem-mysql2 which supports our
    mariadb 10.1.X version. Due to this change we have to refactor the
    current methods to use plain sql.
    
    (cherry picked from commit 31af54e9acbfe91d7be3408df82232f2cfa64a1f)

--- chef/cookbooks/mysql/recipes/client.rb
+++ chef/cookbooks/mysql/recipes/client.rb
@@ -30,7 +30,7 @@ if platform_family?(%w{debian rhel fedora suse})
   package "mysql-ruby" do
     package_name value_for_platform_family(
       ["rhel", "fedora"] => "ruby-mysql",
-      "suse" => "ruby#{node["languages"]["ruby"]["version"].to_f}-rubygem-mysql",
+      "suse" => "ruby#{node["languages"]["ruby"]["version"].to_f}-rubygem-mysql2",
       "debian" => "libmysql-ruby",
       "default" => "libmysql-ruby"
     )
