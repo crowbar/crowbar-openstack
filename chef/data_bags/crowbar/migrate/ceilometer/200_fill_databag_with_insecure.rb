806b5949a7435d7cf58aced20298797f46c1a1d6
Itxaka <igarcia@suse.com>
2017-08-03 17:37:37 +0200

    migrations: Add migrations for the new databag search
    As we are moving to reduce the searches for nodes and instead
    using databags, we need a way to fill those databags if the
    barclamp has already beeen deployed.
    
    This migrations will check if the barclamp has been deployed and
    if it is, it will try to fill the databag with the current values

---
+++ chef/data_bags/crowbar/migrate/ceilometer/200_fill_databag_with_insecure.rb
@@ -0,0 +1,24 @@
+def upgrade(ta, td, a, d)
+  # Check if the proposal is active. If its not then we dont need to do anything
+  # with the databag as it will be filled by the deployment
+  prop = Proposal.where(barclamp: "ceilometer").first
+  unless prop.nil?
+    return a, d unless prop.active?
+  end
+  # Find the role, get all the values and save the databag
+  role = RoleObject.find_role_by_name("ceilometer-config-default")
+  config = {
+    insecure: a["ssl"]["insecure"]
+  }
+  # as we dont have an old_role here, we just pass the role twice, wont affect anything
+  # as the instance_from_role method has an || to use any of those (old_role or role)
+  instance = Crowbar::DataBagConfig.instance_from_role(role, role)
+  Crowbar::DataBagConfig.save("openstack", instance, "ceilometer", config)
+  return a, d
+end
+
+def downgrade(ta, td, a, d)
+  # There is no current way of removing the databag
+  # not even sure if we should do it as it won't affect anything
+  return a, d
+end
