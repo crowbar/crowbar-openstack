07f8333709a73210ed90bb8e6fb2d7bd7ebe567d
Colleen Murphy <colleen.murphy@suse.com>
2017-08-23 15:19:02 +0200

    nova: Randomize placement password
    Without this change, the nova placement user has a default password of
    "placement", set in attributes.rb, and it is impossible to change it.
    This is a massive security flaw for obvious reasons. This change adds
    the ability to change the placement service password via the raw view of
    the nova barclamp and also ensures it defaults to a random one.
    
    (cherry picked from commit 6a28e53e93ef4a09999d1f277ac6726415f2012c)

--- chef/data_bags/crowbar/migrate/nova/123_placement_api.rb
+++ chef/data_bags/crowbar/migrate/nova/123_placement_api.rb
@@ -6,10 +6,16 @@ def upgrade(ta, td, a, d)
     a["placement_db"]["password"] = service.random_password
   end
 
+  if a["placement_service_password"].nil? || a["placement_service_password"].empty?
+    service = ServiceObject.new "fake-logger"
+    a["placement_service_password"] = service.random_password
+  end
+
   return a, d
 end
 
 def downgrade(ta, td, a, d)
   a.delete("placement_db")
+  a.delete("placement_service_password")
   return a, d
 end
