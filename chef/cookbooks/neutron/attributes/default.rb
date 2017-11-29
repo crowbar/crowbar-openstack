8d2f3af0cbfc1bfa607b1e34dc2171f23aa141b8
Jacek Tomasiak <jacek.tomasiak@gmail.com>
2017-11-29 09:39:27 +0100

    neutron: Removed plugin.ini mechanism
    Before introduction of config snippets, symlinks were used to handle
    different plugin configs in /etc/neutron/plugin.ini.
    This mechnism is obsolete as snippets give more flexibility in overriding
    settings.

--- chef/cookbooks/neutron/attributes/default.rb
+++ chef/cookbooks/neutron/attributes/default.rb
@@ -30,6 +30,7 @@ default[:neutron][:lbaas_config_file] = "/etc/neutron/neutron.conf.d/110-neutron
 default[:neutron][:l3_agent_config_file] = "/etc/neutron/neutron-l3-agent.conf.d/100-agent.conf"
 default[:neutron][:metadata_agent_config_file] = "/etc/neutron/neutron-metadata-agent.conf.d/100-metadata_agent.conf"
 default[:neutron][:ml2_config_file] = "/etc/neutron/neutron.conf.d/110-ml2.conf"
+default[:neutron][:nsx_config_file] = "/etc/neutron/neutron.conf.d/110-nsx.conf"
 default[:neutron][:rpc_workers] = 1
 
 default[:neutron][:db][:database] = "neutron"
