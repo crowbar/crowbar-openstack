cf915bbc117df8f9235140e9ce43e1b87a3ea7c7
Dirk Mueller <dmueller@suse.com>
2017-12-20 02:21:21 +0100

    galera: enable client TCP keepalive probes on db connections
    when quickly restarting openstack services due to e.g. a chef
    run or debugging something, the database loadbalancer quickly runs
    out of available db connections because it keeps all connections
    open for the default idle time of 3 hours. keepalive should
    reduce that quite a bit.
    
    (cherry picked from commit 74a58c1f9b1823211be147f1e8ce1b00c4e34ad4)

--- chef/cookbooks/mysql/recipes/ha_galera.rb
+++ chef/cookbooks/mysql/recipes/ha_galera.rb
@@ -340,10 +340,10 @@ haproxy_loadbalancer "galera" do
   # leave some room for pacemaker health checks
   max_connections node[:database][:mysql][:max_connections] - 10
   if node[:mysql][:ha][:clustercheck]
-    options ["httpchk"]
+    options ["httpchk", "clitcpka"]
     default_server "port 5555"
   else
-    options ["mysql-check user monitoring"]
+    options ["mysql-check user monitoring", "clitcpka"]
   end
   stick ({ "on" => "dst" })
   servers ha_servers
