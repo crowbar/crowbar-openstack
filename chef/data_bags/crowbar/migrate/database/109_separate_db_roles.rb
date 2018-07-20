2a27ad03736d5f3a7ceb5d5f4d42fcfec063e6f4
Jiří Suchomel <jiri.suchomel@suse.com>
2018-07-20 16:45:20 +0200

    database: Migration fixes for separate DB roles
    Fix for correct copying of the HA settings.
    Also, some comments were added.

--- chef/data_bags/crowbar/migrate/database/109_separate_db_roles.rb
+++ chef/data_bags/crowbar/migrate/database/109_separate_db_roles.rb
@@ -1,18 +1,25 @@
 def upgrade(ta, td, a, d)
   db_engine = a["sql_engine"]
-  a[db_engine]["ha"] = a["ha"]
-  a.delete("ha")
+
+  # 'ha' hash needs to be moved under 'postgresql' to keep it consistent with mysql
   if db_engine == "postgresql"
-    a["mysql"]["ha"] = ta["mysql"]["ha"]
+    a["postgresql"]["ha"] = a["ha"]
   else
     a["postgresql"]["ha"] = ta["postgresql"]["ha"]
   end
+  a.delete("ha") if a.key? "ha"
+
   d["element_states"] = td["element_states"]
   d["element_order"] = td["element_order"]
 
-  if a["sql_engine"] == "mysql"
+  if db_engine == "mysql"
+    # For the time of upgrade, we're adding new 'mysql-server role', while old 'database-server'
+    # is reserved for existing postgresql setup.
+    # For users that already have mysql (mariadb) deployed with 'database-server' role, we need to
+    # adapt the role assignments so the code that is looking for 'mysql-server' instances always finds
+    # correct mysql nodes.
     d["elements"]["mysql-server"] = d["elements"]["database-server"]
-    d["elements"]["atabase-server"] = []
+    d["elements"]["database-server"] = []
     if d.fetch("elements_expanded", {}).key? "database-server"
       d["elements_expanded"]["mysql-server"] = d["elements_expanded"]["database-server"]
       d["elements_expanded"].delete("database-server")
