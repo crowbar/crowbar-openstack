7599a5a357e1f9467362288e7fa428628f2fb906
Steve Kowalik <steven@wedontsleep.org>
2018-10-12 17:31:52 +1100

    nova/neutron: Fix typo in migration typo patch
    5850c53 fixed typos in the nova and neutron migrations, and included a
    typo in the return value, correct it.

--- chef/data_bags/crowbar/migrate/nova/210_add_default_log_levels.rb
+++ chef/data_bags/crowbar/migrate/nova/210_add_default_log_levels.rb
@@ -1,11 +1,11 @@
 def upgrade(template_attributes, template_deployment, attributes, deployment)
   key = "default_log_levels"
   attributes[key] = template_attributes[key] unless attributes.key? key
-  return attributes, deploment
+  return attributes, deployment
 end
 
 def downgrade(template_attributes, template_deployment, attributes, deployment)
   key = "default_log_levels"
   attributes.delete(key) unless template_attributes.key? key
-  return attributes, deploment
+  return attributes, deployment
 end
