d918647f9feb4f894b0d68b68868be2aee2c82fb
Walter A. Boring IV <waboring@hemna.com>
2018-12-04 13:16:02 +0000

    Change ' to "
    This patch changes the ' to " for the require line.

--- crowbar_framework/app/models/ses_service.rb
+++ crowbar_framework/app/models/ses_service.rb
@@ -13,7 +13,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-require 'securerandom'
+require "securerandom"
 
 class SesService < OpenstackServiceObject
   def initialize(thelogger = nil)
