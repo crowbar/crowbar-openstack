5920ad38420e355faab7a198e84ce8369539e4f4
Dirk Mueller <dmueller@suse.com>
2019-01-03 19:24:57 +0100

    nova: reduce nesting of ceph management code
    Remove one level of madness nesting in the ruby_block
    by moving the over-arching if into an only_if condition
    on the ruby_block

--- chef/cookbooks/nova/recipes/ceph.rb
+++ chef/cookbooks/nova/recipes/ceph.rb
@@ -93,105 +93,102 @@ cinder_controller[:cinder][:volumes].each_with_index do |volume, volid|
 
   ruby_block "save nova key as libvirt secret" do
     block do
-      # Check if libvirt is installed and started
-      if system("virsh hostname &> /dev/null")
+      # First remove conflicting secrets due to same usage name
+      virsh_secret = Mixlib::ShellOut.new("virsh secret-list")
+      secret_list = virsh_secret.run_command.stdout
+      virsh_secret.error!
 
-        # First remove conflicting secrets due to same usage name
-        virsh_secret = Mixlib::ShellOut.new("virsh secret-list")
-        secret_list = virsh_secret.run_command.stdout
-        virsh_secret.error!
+      secret_lines = secret_list.strip.split("\n")
+      if secret_lines.length < 2 || !secret_lines[0].lstrip.start_with?("UUID") || !secret_lines[1].start_with?("----")
+        raise "cannot fetch list of libvirt secret"
+      end
+      secret_lines.shift(2)
 
-        secret_lines = secret_list.strip.split("\n")
-        if secret_lines.length < 2 || !secret_lines[0].lstrip.start_with?("UUID") || !secret_lines[1].start_with?("----")
-          raise "cannot fetch list of libvirt secret"
-        end
-        secret_lines.shift(2)
+      secret_lines.each do |secret_line|
+        secret_uuid = secret_line.split(" ")[0]
+        cmd = ["virsh", "secret-dumpxml", secret_uuid]
+        virsh_secret_dumpxml = Mixlib::ShellOut.new(cmd)
+        secret_xml = virsh_secret_dumpxml.run_command.stdout
+        # some secrets might not be ceph-related, skip these
+        next if secret_xml.index("<usage type='ceph'>").nil?
 
-        secret_lines.each do |secret_line|
-          secret_uuid = secret_line.split(" ")[0]
-          cmd = ["virsh", "secret-dumpxml", secret_uuid]
-          virsh_secret_dumpxml = Mixlib::ShellOut.new(cmd)
-          secret_xml = virsh_secret_dumpxml.run_command.stdout
-          # some secrets might not be ceph-related, skip these
-          next if secret_xml.index("<usage type='ceph'>").nil?
+        # lazy xml parsing
+        re_match = %r[<usage type='ceph'>.*<name>(.*)</name>]m.match(secret_xml)
+        next if re_match.nil?
+        secret_usage = re_match[1]
+        undefine = false
 
-          # lazy xml parsing
-          re_match = %r[<usage type='ceph'>.*<name>(.*)</name>]m.match(secret_xml)
-          next if re_match.nil?
-          secret_usage = re_match[1]
-          undefine = false
-
-          if secret_uuid == rbd_uuid
-            undefine = true if secret_usage != "crowbar-#{rbd_uuid} secret"
-          else
-            undefine = true if secret_usage == "crowbar-#{rbd_uuid} secret"
-          end
-
-          if undefine
-            cmd = ["virsh", "secret-undefine", secret_uuid]
-            virsh_secret_undefine = Mixlib::ShellOut.new(cmd)
-            virsh_secret_undefine.run_command
-          end
-        end
-
-        if !admin_keyring.empty? && File.exist?(admin_keyring)
-          # Now add our secret and its value
-          cmd = [
-            "ceph",
-            "-k", admin_keyring,
-            "-c", ceph_conf,
-            "auth",
-            "get-or-create-key",
-            "client.#{rbd_user}"
-          ]
-
-          ceph_get_key = Mixlib::ShellOut.new(cmd)
-          client_key = ceph_get_key.run_command.stdout.strip
-          ceph_get_key.error!
+        if secret_uuid == rbd_uuid
+          undefine = true if secret_usage != "crowbar-#{rbd_uuid} secret"
         else
-          # Check if rbd keyring was uploaded manually by user
-          client_keyring = "/etc/ceph/ceph.client.#{rbd_user}.keyring"
-          if File.exist?(client_keyring)
-            f = File.open(client_keyring)
-            f.each do |line|
-              if match = line.match("key\s*=\s*(.+)")
-                client_key = match[1]
-                break
-              end
+          undefine = true if secret_usage == "crowbar-#{rbd_uuid} secret"
+        end
+
+        if undefine
+          cmd = ["virsh", "secret-undefine", secret_uuid]
+          virsh_secret_undefine = Mixlib::ShellOut.new(cmd)
+          virsh_secret_undefine.run_command
+        end
+      end
+
+      if !admin_keyring.empty? && File.exist?(admin_keyring)
+        # Now add our secret and its value
+        cmd = [
+          "ceph",
+          "-k", admin_keyring,
+          "-c", ceph_conf,
+          "auth",
+          "get-or-create-key",
+          "client.#{rbd_user}"
+        ]
+
+        ceph_get_key = Mixlib::ShellOut.new(cmd)
+        client_key = ceph_get_key.run_command.stdout.strip
+        ceph_get_key.error!
+      else
+        # Check if rbd keyring was uploaded manually by user
+        client_keyring = "/etc/ceph/ceph.client.#{rbd_user}.keyring"
+        if File.exist?(client_keyring)
+          f = File.open(client_keyring)
+          f.each do |line|
+            if match = line.match("key\s*=\s*(.+)")
+              client_key = match[1]
+              break
             end
           end
         end
+      end
 
-        cmd = ["virsh", "secret-get-value", rbd_uuid]
-        virsh_secret_get_value = Mixlib::ShellOut.new(cmd)
-        secret = virsh_secret_get_value.run_command.stdout.chomp.strip
+      cmd = ["virsh", "secret-get-value", rbd_uuid]
+      virsh_secret_get_value = Mixlib::ShellOut.new(cmd)
+      secret = virsh_secret_get_value.run_command.stdout.chomp.strip
 
-        if secret != client_key
-          secret_file_path = "/etc/ceph/ceph-secret-#{rbd_uuid}.xml"
-          secret_file_content = "<secret ephemeral='no' private='no'>" \
-                                " <uuid>#{rbd_uuid}</uuid>" \
-                                " <usage type='ceph'>" \
-                                " <name>crowbar-#{rbd_uuid} secret</name>" \
-                                " </usage> " \
-                                "</secret>"
-          File.write(secret_file_path, secret_file_content)
+      if secret != client_key
+        secret_file_path = "/etc/ceph/ceph-secret-#{rbd_uuid}.xml"
+        secret_file_content = "<secret ephemeral='no' private='no'>" \
+                              " <uuid>#{rbd_uuid}</uuid>" \
+                              " <usage type='ceph'>" \
+                              " <name>crowbar-#{rbd_uuid} secret</name>" \
+                              " </usage> " \
+                              "</secret>"
+        File.write(secret_file_path, secret_file_content)
 
-          cmd = ["virsh", "secret-define", "--file", secret_file_path]
-          virsh_secret_define = Mixlib::ShellOut.new(cmd)
-          secret_uuid_out = virsh_secret_define.run_command.stdout
+        cmd = ["virsh", "secret-define", "--file", secret_file_path]
+        virsh_secret_define = Mixlib::ShellOut.new(cmd)
+        secret_uuid_out = virsh_secret_define.run_command.stdout
 
-          if secret_uuid_out.scan(/(\S{8}-\S{4}-\S{4}-\S{4}-\S{12})/)
-            cmd = ["virsh", "secret-set-value", "--secret", rbd_uuid, "--base64", client_key]
-            virsh_secret_set_value = Mixlib::ShellOut.new(cmd)
-            virsh_secret_set_value.run_command
-            virsh_secret_set_value.error!
-          else
-            raise "Libvirt secret for UUID #{rbd_uuid} was not created properly."
-          end
-
-          File.delete(secret_file_path)
+        if secret_uuid_out.scan(/(\S{8}-\S{4}-\S{4}-\S{4}-\S{12})/)
+          cmd = ["virsh", "secret-set-value", "--secret", rbd_uuid, "--base64", client_key]
+          virsh_secret_set_value = Mixlib::ShellOut.new(cmd)
+          virsh_secret_set_value.run_command
+          virsh_secret_set_value.error!
+        else
+          raise "Libvirt secret for UUID #{rbd_uuid} was not created properly."
         end
+
+        File.delete(secret_file_path)
       end
     end
+    only_if { system("virsh hostname &> /dev/null") }
   end
 end
