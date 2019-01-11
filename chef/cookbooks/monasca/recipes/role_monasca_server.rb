0e8ed2a4e6168e4d9069bcc22cf6d6dcd960bea9
Thomas Bechtold <tbechtold@suse.com>
2019-01-11 10:03:37 +0100

    monasca: Deploy kibana with Crowbar
    Instead of using monasca-installer for the kibana setup, use Crowbar.
    Note that the kibana setup currently only allows a single kibana
    instance (no cluster). That's the same as with monasca-installer (so
    no regression).
    This is another step in removing monasca-installer.

--- chef/cookbooks/monasca/recipes/role_monasca_server.rb
+++ chef/cookbooks/monasca/recipes/role_monasca_server.rb
@@ -21,6 +21,7 @@ if CrowbarRoleRecipe.node_state_valid_for_role?(node, "monasca", "monasca-server
   include_recipe "#{@cookbook_name}::kafka"
   include_recipe "#{@cookbook_name}::elasticsearch"
   include_recipe "#{@cookbook_name}::elasticsearch_curator"
+  include_recipe "#{@cookbook_name}::kibana"
   include_recipe "#{@cookbook_name}::server"
   include_recipe "#{@cookbook_name}::monitor_monasca"
 end
