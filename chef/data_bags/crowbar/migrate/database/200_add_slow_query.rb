6a93fb89390dd510f52786d3978a953564411d5b
Ralf Haferkamp <rhafer@suse.de>
2019-01-24 15:16:49 +0100

    database: Fix backport schema migrations actualy safe for backporting
    Schema revision 200 to 203 have been backported to older release
    branches. In order to not loose some configurations values during the
    upgrade those migration needed to have additional checks in the newer
    branches.

--- chef/data_bags/crowbar/migrate/database/200_add_slow_query.rb
+++ chef/data_bags/crowbar/migrate/database/200_add_slow_query.rb
@@ -1,9 +1,11 @@
 def upgrade(ta, td, a, d)
-  a["mysql"]["slow_query_logging"] = ta["mysql"]["slow_query_logging"]
+  unless a["mysql"].key? "slow_query_logging"
+    a["mysql"]["slow_query_logging"] = ta["mysql"]["slow_query_logging"]
+  end
   return a, d
 end
 
 def downgrade(ta, td, a, d)
-  a["mysql"].delete("slow_query_logging")
+  a["mysql"].delete("slow_query_logging") unless ta["mysql"].key? "slow_query_logging"
   return a, d
 end
