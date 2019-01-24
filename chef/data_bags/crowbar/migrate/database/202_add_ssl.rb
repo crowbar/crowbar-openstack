b221e8d63169a85fccd5eb3b7a564b8b1740babf
Ralf Haferkamp <rhafer@suse.de>
2019-01-24 15:16:49 +0100

    database: Fix backport schema migrations actualy safe for backporting
    Schema revision 200 to 203 have been backported to older release
    branches. In order to not loose some configurations values during the
    upgrade those migration needed to have additional checks in the newer
    branches.
    
    (cherry picked from commit 6a93fb89390dd510f52786d3978a953564411d5b)

--- chef/data_bags/crowbar/migrate/database/202_add_ssl.rb
+++ chef/data_bags/crowbar/migrate/database/202_add_ssl.rb
@@ -1,9 +1,9 @@
 def upgrade(ta, td, a, d)
-  a["mysql"]["ssl"] = ta["mysql"]["ssl"]
+  a["mysql"]["ssl"] = ta["mysql"]["ssl"] unless a["mysql"].key? "ssl"
   return a, d
 end
 
 def downgrade(ta, td, a, d)
-  a["mysql"].delete("ssl")
+  a["mysql"].delete("ssl") unless ta["mysql"].key? "ssl"
   return a, d
 end
--- chef/data_bags/crowbar/migrate/database/203_add_expire_logs_days.rb
+++ chef/data_bags/crowbar/migrate/database/203_add_expire_logs_days.rb
@@ -1,9 +1,11 @@
 def upgrade(ta, td, a, d)
-  a["mysql"]["expire_logs_days"] = ta["mysql"]["expire_logs_days"]
+  unless a["mysql"].key? "expire_logs_days"
+    a["mysql"]["expire_logs_days"] = ta["mysql"]["expire_logs_days"]
+  end
   return a, d
 end
 
 def downgrade(ta, td, a, d)
-  a["mysql"].delete("expire_logs_days")
+  a["mysql"].delete("expire_logs_days") unless ta["mysql"].key? "expire_logs_days"
   return a, d
 end
