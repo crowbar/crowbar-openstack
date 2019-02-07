79ee1bd99ff396d20967dae3aca4e83581bf2d78
Dirk Mueller <dmueller@suse.com>
2019-02-07 21:32:45 +0100

    opensttack: Update ordering constraints to wait for galera
    This is fallout from the postgresql to galera transition
    as well as the switch away from pacemaker resources for cloned services.
    
    (cherry picked from commit 5caade3961740ce88f8c1f8a8c6ebd3f00c9a15a)

--- chef/cookbooks/nova/recipes/compute_ha.rb
+++ chef/cookbooks/nova/recipes/compute_ha.rb
@@ -334,9 +334,7 @@ crowbar_pacemaker_order_only_existing "o-#{evacuate_primitive}" do
   #  - cinder is used in case of boot from volume
   #  - neutron agents are used even with DVR, if only to have a DHCP server for
   #    the instance to get an IP address
-  ordering "( postgresql #{rabbit_settings[:pacemaker_resource]} cl-keystone cl-swift-proxy " \
-      "cl-glance-api cl-cinder-api cl-neutron-server cl-neutron-dhcp-agent cl-neutron-l3-agent " \
-      "cl-neutron-metadata-agent cl-nova-api ) #{evacuate_primitive}"
+  ordering "( ms-galera #{rabbit_settings[:pacemaker_resource]} ) #{evacuate_primitive}"
   score "Mandatory"
   action :create
   only_if { CrowbarPacemakerHelper.is_cluster_founder?(node) }
