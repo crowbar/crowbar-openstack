5c24d2949b012c375566124198e20326d155681f
Dirk Mueller <dmueller@suse.com>
2019-05-22 12:01:58 +0200

    neutron: increase interval between checks to 30s
    The agent status is anyway not very quickly updated, and if
    we retry too quickly we still might not have the underlying problem
    fixes. Lets be more conservative and check every 30s instead of 10s.
    
    (cherry picked from commit 59672c89a1845a7463a1093589b8424c63c39664)

--- chef/cookbooks/neutron/attributes/default.rb
+++ chef/cookbooks/neutron/attributes/default.rb
@@ -271,6 +271,6 @@ default[:neutron][:ha][:neutron_l3_ha_service][:timeouts][:router_migration][:te
 default[:neutron][:ha][:neutron_l3_ha_service][:timeouts][:router_migration][:kill] = 120
 default[:neutron][:ha][:neutron_l3_ha_service][:hatool][:program] = "/usr/bin/neutron-ha-tool"
 default[:neutron][:ha][:neutron_l3_ha_service][:hatool][:env] = {}
-default[:neutron][:ha][:neutron_l3_ha_service][:seconds_to_sleep_between_checks] = 10
+default[:neutron][:ha][:neutron_l3_ha_service][:seconds_to_sleep_between_checks] = 30
 default[:neutron][:ha][:neutron_l3_ha_service][:max_errors_tolerated] = 10
 default[:neutron][:ha][:neutron_l3_ha_service][:log_file] = "/var/log/neutron/neutron-l3-ha-service.log"
