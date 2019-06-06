98a020cec11c7f546040414a5966b81822905393
Abel Navarro <abel.navarro@gmail.com>
2019-06-06 10:31:58 +0200

    [5.0] Configurable timeout for Galera pre-sync
    We have a pacemaker sync-mark to wait for all nodes to have all galera
    packages and configuration files installed. The default timeout is 60
    seconds for these sync-marks.
    
    While installing all cluster members at once, like in an initial
    installation, all nodes take about the same time to install the packages
    and reach the sync-mark about the same time as well. But when adding a
    new node to the cluster, the already installed nodes reach the sync-mark
    much faster and the default timeout proves insuficient.
    
    Based on tests in developer environments, this takes roughly 70 seconds.
    We will be setting this to 180 seconds to give enough margin.
    
    (cherry picked from commit 4f1d42ed8beb5e35b4428904adccde5cbd6ab570)

---
+++ chef/data_bags/crowbar/migrate/database/211_add_presync_timeout.rb
@@ -0,0 +1,11 @@
+def upgrade(template_attrs, template_deployment, attrs, deployment)
+  unless attrs["mysql"]["presync_timeout"]
+    attrs["mysql"]["presync_timeout"] = template_attrs["mysql"]["presync_timeout"]
+  end
+  return attrs, deployment
+end
+
+def downgrade(template_attrs, template_deployment, attrs, deployment)
+  attrs["mysql"].delete("presync_timeout") unless template_attrs["mysql"].key?("presync_timeout")
+  return attrs, deployment
+end
