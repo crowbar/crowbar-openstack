adebee791720ae8de3a5dfba42cc2e6f1e039599
Johannes Grassler <johannes.grassler@suse.com>
2019-06-17 17:52:02 +0200

    monasca: use string keys for attrs in migrations
    Symbolic keys cause the migrations to fail during upgrade due
    to these keys not being found. This commit fixes the problem.

--- chef/data_bags/crowbar/migrate/monasca/302_add_mondb.rb
+++ chef/data_bags/crowbar/migrate/monasca/302_add_mondb.rb
@@ -1,20 +1,24 @@
 def upgrade(template_attrs, template_deployment, attrs, deployment)
   unless attrs.key? "db_monapi"
-    attrs[:db_monapi] = template_attrs[:db_monapi]
+    attrs["db_monapi"] = template_attrs[:db_monapi]
     service = ServiceObject.new "fake-logger"
-    attrs[:db_monapi][:password] = service.random_password
+    attrs["db_monapi"]["password"] = service.random_password
   end
+
   # use the database password that is already available
-  if attrs[:master].key? "database_monapi_password"
-    attrs[:db_monapi][:password] = attrs[:master][:database_monapi_password]
-    attrs[:master].delete(:database_monapi_password)
+  if attrs.key? "master"
+    if attrs["master"].key? "database_monapi_password"
+      attrs["db_monapi"]["password"] = attrs["master"]["database_monapi_password"]
+      attrs["master"].delete(:database_monapi_password)
+    end
   end
+
   return attrs, deployment
 end
 
 def downgrade(template_attrs, template_deployment, attrs, deployment)
   # copy password to old place
-  attrs[:master][:database_monapi_password] = attrs[:db_monapi][:password]
-  attrs.delete(:db_monapi)
+  attrs["master"]["database_monapi_password"] = attrs["db_monapi"]["password"]
+  attrs.delete("db_monapi")
   return attrs, deployment
 end
