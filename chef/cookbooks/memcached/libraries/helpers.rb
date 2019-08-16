7ffec8ff80e4ad86751350b74b934c5d0d37de34
Rick Salevsky <rsalevsky@suse.com>
2019-08-16 16:26:41 +0200

    Add additional debug output to memcached (SOC-10173)
    In order to understand the isses which cause SOC-10173 better we need
    addional debug output.

--- chef/cookbooks/memcached/libraries/helpers.rb
+++ chef/cookbooks/memcached/libraries/helpers.rb
@@ -1,8 +1,11 @@
 module MemcachedHelper
   def self.get_memcached_servers(memcached_nodes)
+    Chef::Log.info("Getting memcached servers for #{memcached_nodes.inspect}")
     memcached_servers = memcached_nodes.map do |n|
+      Chef::Log.info("Processing #{n.inspect}")
       node_admin_ip = Chef::Recipe::Barclamp::Inventory.get_network_by_type(n,
                                                                             "admin").address
+      Chef::Log.info("IP #{node_admin_ip}")
       port = if n.key?(:memcached) && n[:memcached].key?(:port)
         n[:memcached][:port]
       else
@@ -10,6 +13,7 @@ module MemcachedHelper
       end
       "#{node_admin_ip}:#{port}"
     end
+    Chef::Log.info("Memcached servers #{memcached_servers.inspect}")
     memcached_servers.sort!
     memcached_servers
   end
