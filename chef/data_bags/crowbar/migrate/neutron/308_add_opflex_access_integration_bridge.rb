3630806c4e88eee50c937114f80004b06dd370f4
Jiří Suchomel <jiri.suchomel@suse.com>
2019-10-23 14:48:57 +0200

    Fix the migration that tried to access Array as a Hash (SOC-10896)
    Commit that introduced the problem: cb5347d6a47565c2ef1aebf10ef80d980114e046

--- chef/data_bags/crowbar/migrate/neutron/308_add_opflex_access_integration_bridge.rb
+++ chef/data_bags/crowbar/migrate/neutron/308_add_opflex_access_integration_bridge.rb
@@ -1,21 +1,26 @@
-def upgrade(tattr, tdep, attr, dep)
-  unless attr["apic"]["opflex"].key?("integration_bridge")
-    attr["apic"]["opflex"]["integration_bridge"] = tattr["apic"]["opflex"]["integration_bridge"]
+def upgrade(tattr, tdep, att, dep)
+  att["apic"]["opflex"].each_with_index do |opflex, i|
+    unless opflex.key?("integration_bridge")
+      att["apic"]["opflex"][i]["integration_bridge"] =
+        tattr["apic"]["opflex"][0]["integration_bridge"]
+    end
+    unless opflex.key?("access_bridge")
+      att["apic"]["opflex"][i]["access_bridge"] =
+        tattr["apic"]["opflex"][0]["access_bridge"]
+    end
   end
-  unless attr["apic"]["opflex"].key?("access_bridge")
-    attr["apic"]["opflex"]["access_bridge"] = tattr["apic"]["opflex"]["access_bridge"]
-  end
-
-  return attr, dep
+  return att, dep
 end
 
-def downgrade(tattr, tdep, attr, dep)
-  unless tattr["apic"]["opflex"].key?("integration_bridge")
-    attr["apic"]["opflex"].delete("integration_bridge") if attr.key?("integration_bridge")
-  end
-  unless tattr["apic"]["opflex"].key?("access_bridge")
-    attr["apic"]["opflex"].delete("access_bridge") if attr.key?("access_bridge")
+def downgrade(tattr, tdep, att, dep)
+  att["apic"]["opflex"].each_with_index do |opflex, i|
+    unless tattr["apic"]["opflex"][0].key?("integration_bridge")
+      att["apic"]["opflex"][i].delete("integration_bridge") if opflex.key?("integration_bridge")
+    end
+    unless tattr["apic"]["opflex"][0].key?("access_bridge")
+      att["apic"]["opflex"][i].delete("access_bridge") if opflex.key?("access_bridge")
+    end
   end
 
-  return attr, dep
+  return att, dep
 end
