cdb69234bb0be76e6382a0a378d9d763bb77a1ed
Nicolas Bock <nicolas.bock@suse.com>
2019-11-25 13:49:16 -0700

    Octavia: Create listener port for health manager (SOC-10904)
    Create a port and interfaces so that the health manager can
    communicate with the `lb-mgmt-net` and the amphorae.
    
    Signed-off-by: Nicolas Bock <nicolas.bock@suse.com>

--- chef/cookbooks/octavia/recipes/nova.rb
+++ chef/cookbooks/octavia/recipes/nova.rb
@@ -89,6 +89,13 @@ execute "create_amphora_image" do
   action :run
 end
 
+# execute "create octavia user key" do
+#   command "#{cmd} keypair create --public-key /root/.ssh/id_rsa.pub octavia-key"
+#   not_if `#{cmd} keypair list | grep --quiet ' octavia-key '`
+#   only_if { !ha_enabled || CrowbarPacemakerHelper.is_cluster_founder?(node) }
+#   action :run
+# end
+
 manage_net = node[:octavia][:amphora][:manage_net]
 manage_cidr = node[:octavia][:amphora][:manage_cidr]
 
@@ -109,6 +116,71 @@ execute "create_octavia_management_subnet" do
   retries 5
   retry_delay 10
   action :run
+end
+
+execute "create listener port for health-manager" do
+  command "#{cmd} port create " \
+          "--network #{manage_net} " \
+          "--security-group #{sec_group} " \
+          "--device-owner Octavia:health-mgr " \
+          "--host=$(hostname) " \
+          "--description='Octavia Health Manager Listener Port' " \
+          "lb-mgmt-listener-port"
+  not_if "$(#{cmd} port list | grep -q ' lb-mgmt-listener-port ')"
+  only_if { !ha_enabled || CrowbarPacemakerHelper.is_cluster_founder?(node) }
+  retries 5
+  retry_delay 10
+  action :run
+end
+
+neutron_server = node_search_with_cache("roles:neutron-server").first
+Chef::Log.info("first neutron_server = #{neutron_server}")
+
+ruby_block "Create interface to bridge to #{manage_net}" do
+  block do
+    port_id = shell_out("#{cmd} port show --column id --format value " \
+                        "lb-mgmt-listener-port").stdout.chomp
+    port_mac = shell_out("#{cmd} port show --column mac_address " \
+                         "--format value lb-mgmt-listener-port").stdout.chomp
+    port_ip = shell_out("#{cmd} port show --column fixed_ips --format value " \
+                        "lb-mgmt-listener-port").stdout.chomp().match(/ip_address='([^']*).*$/)[1]
+
+    Chef::Log.info("port_id = #{port_id}")
+    Chef::Log.info("port_mac = #{port_mac}")
+    Chef::Log.info("port_ip = #{port_ip}")
+
+    Chef::Log.info("server[:neutron][:ml2_mechanism_drivers]: " \
+                   "#{neutron_server[:neutron][:ml2_mechanism_drivers]}")
+
+    if neutron_server[:neutron][:networking_plugin] == "ml2" && \
+       neutron_server[:neutron][:ml2_mechanism_drivers].include?("linuxbridge")
+      shell_out("ip link add o-hm0 type veth peer name o-bhm0") \
+        unless system("ip link show o-hm0 > /dev/null 2>&1")
+      net_id = shell_out("#{cmd} network show #{manage_net} " \
+                         "--column id --format value").stdout.chomp
+      bridge_name = "brq" + net_id[0..10]
+      Chef::Log.info("net_id = #{net_id}")
+      Chef::Log.info("bridge_name = #{bridge_name}")
+      shell_out("brctl addif #{bridge_name} o-bhm0") \
+        unless system("brctl show #{bridge_name} | grep --quiet o-bhm0")
+      shell_out("ip link set o-bhm0 up")
+    else
+      Chef::Log.info("Not using linuxbridge")
+    end
+    Chef::Log.info("Setting MAC address of veth port")
+    shell_out("ip link set dev o-hm0 address #{port_mac}")
+
+    Chef::Log.info("Setting IP address of veth port")
+    shell_out("ip address add #{port_ip}/16 dev o-hm0")
+    shell_out("ip link set o-hm0 up")
+
+    shell_out("iptables --insert INPUT -i o-hm0 -p udp --dport 5555 -j ACCEPT") \
+      unless system("iptables --check INPUT -i o-hm0 -p udp --dport 5555 -j ACCEPT")
+    shell_out("iptables -I INPUT -i o-hm0 -p udp --dport 10514 -j ACCEPT") \
+      unless system("iptables --check INPUT -i o-hm0 -p udp --dport 10514 -j ACCEPT")
+    shell_out("iptables -I INPUT -i o-hm0 -p udp --dport 20514 -j ACCEPT") \
+      unless system("iptables --check INPUT -i o-hm0 -p udp --dport 20514 -j ACCEPT")
+  end
 end
 
 # Installing the amphora image package and creating OpenStack artifacts (the
