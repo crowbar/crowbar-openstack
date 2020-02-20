crowbar/crowbar-openstack

commit 87ad941d55a348b1d83a50fde25e29db40beb559

Do not use obsoleted --endpoint-type option with CLI
Correct option is --os-endpoint-type which by default uses
the value of OS_ENDPOINT_TYPE env var.

chef/cookbooks/neutron/recipes/post_install_conf.rb
@@ -77,7 +77,7 @@ env << "OS_INTERFACE=internal "
 env << "OS_USER_DOMAIN_NAME=Default "
 env << "OS_PROJECT_DOMAIN_NAME=Default "
 env << "OS_IDENTITY_API_VERSION=3"
-neutron_args = "--endpoint-type internalURL"
+env << "OS_ENDPOINT_TYPE=internalURL"
 neutron_args = "#{neutron_args} --insecure" if ssl_insecure
 neutron_cmd = "#{env} neutron #{neutron_args}"
 

commit 0f112917e0720e95bbee898ef26d262b0a7111ca

neutron: Place space between CLI arguments
This was broken in commit 87ad941d55a348b1d83a50fde25e29db40beb559

chef/cookbooks/neutron/recipes/post_install_conf.rb
@@ -76,7 +76,7 @@ env << "OS_REGION_NAME='#{keystone_settings["endpoint_region"]}' "
 env << "OS_INTERFACE=internal "
 env << "OS_USER_DOMAIN_NAME=Default "
 env << "OS_PROJECT_DOMAIN_NAME=Default "
-env << "OS_IDENTITY_API_VERSION=3"
+env << "OS_IDENTITY_API_VERSION=3 "
 env << "OS_ENDPOINT_TYPE=internalURL"
 neutron_args = "#{neutron_args} --insecure" if ssl_insecure
 neutron_cmd = "#{env} neutron #{neutron_args}"